3650420ff12a9e49cb4db3b0f91882faf5f3ed70
cryan <cryan>
2007-09-17 20:03:27 +0000

    Fixed follow up comment about menu item not working. Issue # 5812.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@84 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/dspRunningAvailability.cpp
+++ guiclient/dspRunningAvailability.cpp
@@ -235,8 +235,8 @@ void dspRunningAvailability::sSoftenOrder()
 void dspRunningAvailability::sReleaseOrder()
 {
   // TODO
-  if (_availability->currentItem()->text(ORDERTYPE_COL) == tr("Planned W/O (firmed)") ||
-      _availability->currentItem()->text(ORDERTYPE_COL) == tr("Planned W/O"))
+  if (_availability->currentItem()->text(ORDERNUM_COL) == tr("Planned W/O (firmed)") ||
+      _availability->currentItem()->text(ORDERNUM_COL) == tr("Planned W/O"))
   {
     ParameterList params;
     params.append("mode", "release");
@@ -253,8 +253,8 @@ void dspRunningAvailability::sReleaseOrder()
     }
 #endif
   }
-  else if (_availability->currentItem()->text(ORDERTYPE_COL) == tr("Planned P/O (firmed)") ||
-	  _availability->currentItem()->text(ORDERTYPE_COL) == tr("Planned P/O"))
+  else if (_availability->currentItem()->text(ORDERNUM_COL) == tr("Planned P/O (firmed)") ||
+	  _availability->currentItem()->text(ORDERNUM_COL) == tr("Planned P/O"))
   {
     ParameterList params;
     params.append("mode", "release");
