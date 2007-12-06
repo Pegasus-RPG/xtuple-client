0a0f99399bb4234f46fc23ed33a8ea93a208dbf5
cryan <cryan>
2007-12-06 13:27:36 +0000

    Updated version in preparation for release.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@840 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -68,8 +68,8 @@ QString _Name = " Client";
 #endif
 
 #ifndef __USEALTVERSION__
-QString _Version   = "2.3.0 Beta 1";
-QString _dbVersion = "2.3.0Beta1";
+QString _Version   = "2.3.0 Beta 2";
+QString _dbVersion = "2.3.0Beta2";
 #else
 #include "../altVersion.cpp"
 #endif
