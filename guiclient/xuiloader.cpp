4a05cecce2f87758489a4197055925891f08ff0e
cryan <cryan>
2008-04-18 17:56:45 +0000

    new class to handle loading ui files with support for our custom widgets.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@1917 ea1a15ef-c238-0410-93cb-fa28e9f07733

---
+++ guiclient/xuiloader.cpp
@@ -0,0 +1,118 @@
+/*
+ * Common Public Attribution License Version 1.0. 
+ * 
+ * The contents of this file are subject to the Common Public Attribution 
+ * License Version 1.0 (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License 
+ * at http://www.xTuple.com/CPAL.  The License is based on the Mozilla 
+ * Public License Version 1.1 but Sections 14 and 15 have been added to 
+ * cover use of software over a computer network and provide for limited 
+ * attribution for the Original Developer. In addition, Exhibit A has 
+ * been modified to be consistent with Exhibit B.
+ * 
+ * Software distributed under the License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See 
+ * the License for the specific language governing rights and limitations 
+ * under the License. 
+ * 
+ * The Original Code is PostBooks Accounting, ERP, and CRM Suite. 
+ * 
+ * The Original Developer is not the Initial Developer and is __________. 
+ * If left blank, the Original Developer is the Initial Developer. 
+ * The Initial Developer of the Original Code is OpenMFG, LLC, 
+ * d/b/a xTuple. All portions of the code written by xTuple are Copyright 
+ * (c) 1999-2008 OpenMFG, LLC, d/b/a xTuple. All Rights Reserved. 
+ * 
+ * Contributor(s): ______________________.
+ * 
+ * Alternatively, the contents of this file may be used under the terms 
+ * of the xTuple End-User License Agreeement (the xTuple License), in which 
+ * case the provisions of the xTuple License are applicable instead of 
+ * those above.  If you wish to allow use of your version of this file only 
+ * under the terms of the xTuple License and not to allow others to use 
+ * your version of this file under the CPAL, indicate your decision by 
+ * deleting the provisions above and replace them with the notice and other 
+ * provisions required by the xTuple License. If you do not delete the 
+ * provisions above, a recipient may use your version of this file under 
+ * either the CPAL or the xTuple License.
+ * 
+ * EXHIBIT B.  Attribution Information
+ * 
+ * Attribution Copyright Notice: 
+ * Copyright (c) 1999-2008 by OpenMFG, LLC, d/b/a xTuple
+ * 
+ * Attribution Phrase: 
+ * Powered by PostBooks, an open source solution from xTuple
+ * 
+ * Attribution URL: www.xtuple.org 
+ * (to be included in the "Community" menu of the application if possible)
+ * 
+ * Graphic Image as provided in the Covered Code, if any. 
+ * (online at www.xtuple.com/poweredby)
+ * 
+ * Display of Attribution Information is required in Larger Works which 
+ * are defined in the CPAL as a work which combines Covered Code or 
+ * portions thereof with code not governed by the terms of the CPAL.
+ */
+
+#include "xuiloader.h"
+
+#include <QPluginLoader>
+#include <QStackedWidget>
+#include <QTabWidget>
+#include <QDialog>
+#include <QToolBox>
+
+XUiLoader::XUiLoader(QObject* parent)
+  : QUiLoader(parent)
+{
+  QObjectList instances = QPluginLoader::staticInstances();
+  for (int i = 0; i < instances.count(); ++i)
+  {
+    // step 1) try with a normal plugin
+    QDesignerCustomWidgetInterface *iface = qobject_cast<QDesignerCustomWidgetInterface *>(instances.at(i));
+    if (iface != 0) {
+      m_customWidgets.insert(iface->name(), iface);
+      continue;
+    }
+
+    // step 2) try with a collection of plugins
+    QDesignerCustomWidgetCollectionInterface *c = qobject_cast<QDesignerCustomWidgetCollectionInterface *>(instances.at(i));
+    if (c != 0) {
+      foreach (QDesignerCustomWidgetInterface *iface, c->customWidgets()) {
+        m_customWidgets.insert(iface->name(), iface);
+      }
+    }
+  }
+}
+
+XUiLoader::~XUiLoader()
+{
+}
+
+QWidget * XUiLoader::createWidget ( const QString & className, QWidget * parent, const QString & name )
+{
+  QWidget *w = 0;
+
+  if (qobject_cast<QTabWidget*>(parent)
+        || qobject_cast<QStackedWidget*>(parent)
+        || qobject_cast<QToolBox*>(parent))
+    parent = 0;
+
+  QDesignerCustomWidgetInterface *factory = m_customWidgets.value(className);
+  if (factory != 0)
+    w = factory->createWidget(parent);
+
+  if(w)
+  {
+    w->setObjectName(name);
+
+    if (qobject_cast<QDialog *>(w))
+      w->setParent(parent);
+
+    return w;
+  }
+
+  return QUiLoader::createWidget(className, parent, name);
+}
+
