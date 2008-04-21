9c92eacd13beff42e135e98f8f7277c9b3f42cc6
gilmoskowitz <gilmoskowitz>
2008-04-21 13:59:34 +0000

    issue #6105, ~0012471 - improve sort behavior by converting to new XTreeWidget
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@1921 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/incident.cpp
+++ guiclient/incident.cpp
@@ -98,16 +98,16 @@ incident::incident(QWidget* parent, const char* name, bool modal, Qt::WFlags fl)
   _category->setType(XComboBox::IncidentCategory);
   _lotserial->setStrict(false);
 
-  _incdthist->addColumn(tr("Username"),    _userColumn,     Qt::AlignLeft );
-  _incdthist->addColumn(tr("Date/Time"),   _timeDateColumn, Qt::AlignLeft );
-  _incdthist->addColumn(tr("Description"), -1,              Qt::AlignLeft );
+  _incdthist->addColumn(tr("Username"),     _userColumn, Qt::AlignLeft, true, "incdthist_username");
+  _incdthist->addColumn(tr("Date/Time"),_timeDateColumn, Qt::AlignLeft, true, "incdthist_timestamp");
+  _incdthist->addColumn(tr("Description"),           -1, Qt::AlignLeft, true, "incdthist_descrip");
 
-  _todoList->addColumn(tr("Seq"),		 25,	Qt::AlignRight);
-  _todoList->addColumn(tr("User"),	_userColumn,	Qt::AlignLeft );
-  _todoList->addColumn(tr("Name"),		100,	Qt::AlignLeft );
-  _todoList->addColumn(tr("Description"),	 -1,	Qt::AlignLeft );
-  _todoList->addColumn(tr("Status"),  _statusColumn,	Qt::AlignLeft );
-  _todoList->addColumn(tr("Due Date"),	_dateColumn,	Qt::AlignLeft );
+  _todoList->addColumn(tr("Seq"),		 25,	Qt::AlignRight, true, "todoitem_seq");
+  _todoList->addColumn(tr("User"),	_userColumn,	Qt::AlignLeft,  true, "usr_username");
+  _todoList->addColumn(tr("Name"),		100,	Qt::AlignLeft,  true, "todoitem_name");
+  _todoList->addColumn(tr("Description"),	 -1,	Qt::AlignLeft,  true, "todoitem_notes");
+  _todoList->addColumn(tr("Status"),  _statusColumn,	Qt::AlignLeft,  true, "todoitem_status");
+  _todoList->addColumn(tr("Due Date"),	_dateColumn,	Qt::AlignLeft,  true, "todoitem_due_date");
 
   q.prepare("SELECT usr_id "
 	    "FROM usr "
@@ -431,10 +431,7 @@ bool incident::save(bool partial)
 
 void incident::sFillHistoryList()
 {
-  q.prepare("SELECT incdthist_id,"
-            "       incdthist_username,"
-            "       formatDateTime(incdthist_timestamp),"
-            "       incdthist_descrip "
+  q.prepare("SELECT * "
             "  FROM incdthist"
             " WHERE (incdthist_incdt_id=:incdt_id)"
             " ORDER BY incdthist_timestamp; ");
@@ -450,6 +447,11 @@ void incident::sFillHistoryList()
   q.bindValue(":contact", tr("Contact"));
   q.exec();
   _incdthist->populate(q);
+  if (q.lastError().type() != QSqlError::None)
+  {
+    systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+    return;
+  }
 }
 
 void incident::populate()
@@ -573,44 +575,26 @@ void incident::sDeleteTodoItem()
 
 void incident::sFillTodoList()
 {
-  XTreeWidgetItem* last = 0;
-  q.prepare("SELECT todoitem_id, todoitem_usr_id, todoitem_seq, "
-	    "       usr_username, todoitem_name, "
+  q.prepare("SELECT todoitem_id, todoitem_usr_id, *, "
 	    "       firstLine(todoitem_notes) AS todoitem_notes, "
-	    "       todoitem_status, todoitem_due_date "
+            "       CASE WHEN (todoitem_status != 'C' AND"
+            "                  todoitem_due_date < CURRENT_DATE) THEN 'expired'"
+            "            WHEN (todoitem_status != 'C' AND"
+            "                  todoitem_due_date > CURRENT_DATE) THEN 'future'"
+            "       END AS todoitem_due_date_qtforegroundrole "
 	    "FROM usr, todoitem "
 	    "WHERE ( (todoitem_incdt_id=:incdt_id) "
 	    "  AND   (todoitem_usr_id=usr_id)"
 	    "  AND   (todoitem_active) ) "
-	    "ORDER BY todoitem_seq, usr_username;");
+	    "ORDER BY todoitem_due_date, todoitem_seq, usr_username;");
 
   q.bindValue(":incdt_id", _incdtid);
   q.exec();
+  _todoList->populate(q, true);
   if (q.lastError().type() != QSqlError::None)
   {
     systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
     return;
-  }
-  _todoList->clear();
-  while (q.next())
-  {
-    last = new XTreeWidgetItem(_todoList, last,
-			     q.value("todoitem_id").toInt(),
-			     q.value("todoitem_usr_id").toInt(),
-			     q.value("todoitem_seq").toString(),
-			     q.value("usr_username").toString(),
-			     q.value("todoitem_name").toString(),
-			     q.value("todoitem_notes").toString(),
-			     q.value("todoitem_status").toString(),
-			     q.value("todoitem_due_date").isNull() ? "" :
-				    q.value("todoitem_due_date").toString());
-    if (q.value("todoitem_status") != "C")
-    {
-      if (q.value("todoitem_due_date").toDate() < QDate::currentDate())
-	last->setTextColor("red");
-      else if (q.value("todoitem_due_date").toDate() > QDate::currentDate())
-	last->setTextColor("green");
-    }
   }
 }
 
