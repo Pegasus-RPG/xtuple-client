8ade6a4454113c75a60bbbba5d1df3991e196c6c
jrogelstad <jrogelstad>
2008-07-24 13:22:14 +0000

    issue #7034: Correct query so transfer receipt does not see outbound trans as duplicate SN.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@2763 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/createLotSerial.cpp
+++ guiclient/createLotSerial.cpp
@@ -271,7 +271,7 @@ void createLotSerial::sAssign()
 		  "FROM itemlocdist,itemsite,ls "
 		  "WHERE ((itemlocdist_itemsite_id=:itemsite_id) "
                   "  AND (itemlocdist_itemsite_id=itemsite_id)"
-                  "  AND (itemsite_item_id=ls_item_id)"
+                  "  AND (itemlocdist_ls_id=ls_id)"
 		  "  AND (ls_number=:lotserial) "
 		  "  AND (itemlocdist_source_type='D'))) as data;");
     q.bindValue(":itemsite_id", _itemsiteid);
