5ed91483398c43a359f9ace3013cc5e5837138a9
gilmoskowitz <gilmoskowitz>
2008-09-23 19:06:04 +0000

    bug #3146 and bug #6256 - new xtreewidget stuff
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@3520 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/creditMemoEditList.cpp
+++ guiclient/creditMemoEditList.cpp
@@ -59,6 +59,7 @@
 
 #include <QMenu>
 #include <QMessageBox>
+#include <QSqlError>
 #include <QVariant>
 
 #include <openreports.h>
@@ -77,15 +78,14 @@ creditMemoEditList::creditMemoEditList(QWidget* parent, const char* name, Qt::WF
   connect(_cmhead, SIGNAL(populateMenu(QMenu*,QTreeWidgetItem*,int)), this, SLOT(sPopulateMenu(QMenu*,QTreeWidgetItem*)));
   connect(_print, SIGNAL(clicked()), this, SLOT(sPrint()));
 
-  _cmhead->setRootIsDecorated(TRUE);
-  _cmhead->addColumn(tr("Document #"),       (_itemColumn+ _cmhead->indentation()),  Qt::AlignLeft   );
-  _cmhead->addColumn(tr("Order #"),          _orderColumn,                            Qt::AlignLeft   );
-  _cmhead->addColumn(tr("Cust./Item #"),     _itemColumn,                             Qt::AlignLeft   );
-  _cmhead->addColumn(tr("Name/Description"), -1,                                      Qt::AlignLeft   );
-  _cmhead->addColumn(tr("UOM"),              _uomColumn,                              Qt::AlignCenter );
-  _cmhead->addColumn(tr("Qty. to Bill"),     _qtyColumn,                              Qt::AlignRight  );
-  _cmhead->addColumn(tr("Price"),            _costColumn,                             Qt::AlignRight  );
-  _cmhead->addColumn(tr("Ext. Price"),       _moneyColumn,                            Qt::AlignRight  );
+  _cmhead->addColumn(tr("Document #"), (_itemColumn+_cmhead->indentation()), Qt::AlignLeft,  true, "docnumber");
+  _cmhead->addColumn(tr("Order #"),     _orderColumn, Qt::AlignLeft,  true, "ordernumber");
+  _cmhead->addColumn(tr("Cust./Item #"), _itemColumn, Qt::AlignLeft,  true, "custitemnumber");
+  _cmhead->addColumn(tr("Name/Description"),      -1, Qt::AlignLeft,  true, "namedescrip");
+  _cmhead->addColumn(tr("UOM"),           _uomColumn, Qt::AlignCenter,true, "uom_name");
+  _cmhead->addColumn(tr("Qty. to Bill"),  _qtyColumn, Qt::AlignRight, true, "qtytobill");
+  _cmhead->addColumn(tr("Price"),        _costColumn, Qt::AlignRight, true, "price");
+  _cmhead->addColumn(tr("Ext. Price"),  _moneyColumn, Qt::AlignRight, true, "extprice");
 
   connect(omfgThis, SIGNAL(creditMemosUpdated()), this, SLOT(sFillList()));
 
@@ -142,80 +142,133 @@ void creditMemoEditList::sPopulateMenu(QMenu *pMenu, QTreeWidgetItem *pSelected)
 
 void creditMemoEditList::sFillList()
 {
-  _cmhead->clear();
+  q.exec("SELECT *,"
+         "       ('C/M-' || formatCreditMemoNumber(cmhead_id)) AS docnumber,"
+         "       CASE WHEN seq != 0 THEN ''"
+         "       END AS docnumber_qtdisplayrole,"
+         "       'qty' AS qtytobill_xtnumericrole,"
+         "       'price' AS price_xtnumericrole,"
+         "       'curr' AS extprice_xtnumericrole,"
+         "       CASE WHEN namedescrip = 'Not Assigned' THEN 'error'"
+         "       END AS qtforegroundrole "
+         "FROM ("
+         "SELECT cmhead_id, -1 AS altid,"
+         "       cmhead_number AS ordernumber,"
+         "       cust_number AS custitemnumber,"
+         "       cmhead_billtoname AS namedescrip,"
+         "       CAST(NULL AS TEXT) AS uom_name,"
+         "       CAST(NULL AS NUMERIC) AS qtytobill,"
+         "       CAST(NULL AS NUMERIC) AS price,"
+         "       CAST(NULL AS NUMERIC) AS extprice,"
+         "       0 AS cmitem_linenumber, 0 AS xtindentrole, 0 AS seq "
+         "FROM cmhead JOIN custinfo ON (cmhead_cust_id=cust_id) "
+         "WHERE ((NOT cmhead_posted)"
+         "  AND  (NOT cmhead_hold)) "
+         "UNION "
+         "SELECT cmhead_id, cmitem_id,"
+         "       NULL, item_number,"
+         "       item_descrip1,"
+         "       uom_name, cmitem_qtycredit * cmitem_qty_invuomratio,"
+         "       cmitem_unitprice / cmitem_price_invuomratio,"
+         "       cmitem_qtycredit * cmitem_qty_invuomratio *"
+         "                         cmitem_unitprice / cmitem_price_invuomratio,"
+         "       cmitem_linenumber, 1 AS xtindentrole, 1 AS seq "
+         "FROM cmhead, cmitem, itemsite, item, uom "
+         "WHERE ((NOT cmhead_posted)"
+         "   AND (NOT cmhead_hold)"
+         "   AND (cmhead_id=cmitem_cmhead_id)"
+         "   AND (cmitem_itemsite_id=itemsite_id)"
+         "   AND (itemsite_item_id=item_id)"
+         "   AND (item_inv_uom_id=uom_id)) "
+         "UNION "
+         "SELECT cmhead_id, -1,"
+         "       NULL, 'Debit',"
+         "       COALESCE((SELECT formatGLAccountLong(accnt_id)"
+         "                 FROM accnt, salesaccnt"
+         "                 WHERE ((salesaccnt_sales_accnt_id=accnt_id)"
+         "                    AND (salesaccnt_id=findSalesAccnt(itemsite_id, "
+         "                               cmhead_cust_id)))), 'Not Assigned'),"
+         "       NULL, NULL, NULL,"
+         "       cmitem_qtycredit * cmitem_qty_invuomratio *"
+         "                         cmitem_unitprice / cmitem_price_invuomratio,"
+         "       cmitem_linenumber, 2 AS xtindentrole, 2 AS seq "
+         "FROM cmhead, cmitem, itemsite, item, uom "
+         "WHERE ((NOT cmhead_posted)"
+         "   AND (NOT cmhead_hold)"
+         "   AND (cmhead_id=cmitem_cmhead_id)"
+         "   AND (cmitem_itemsite_id=itemsite_id)"
+         "   AND (itemsite_item_id=item_id)"
+         "   AND (item_inv_uom_id=uom_id)) "
 
-  q.exec("SELECT * FROM creditMemoEditList;");
-  if (q.first())
+         "UNION "
+         "SELECT cmhead_id, -1,"
+         "       NULL, 'Debit',"
+         "       CASE WHEN (accnt_id IS NULL) THEN 'Not Assigned'"
+         "            ELSE formatGLAccountLong(accnt_id)"
+         "       END,"
+         "       NULL, NULL, cmhead_freight,"
+         "       cmhead_freight,"
+         "       99999, 1 AS xtindentrole, 3 AS seq "
+         "FROM cmhead LEFT OUTER JOIN"
+         "     accnt ON (accnt_id=findFreightAccount(cmhead_cust_id)) "
+         "WHERE ((NOT cmhead_posted)"
+         "   AND (NOT cmhead_hold)"
+         "   AND (cmhead_freight <> 0))"
+
+         "UNION "
+         "SELECT cmhead_id, -1,"
+         "       NULL, 'Debit',"
+         "       formatGLAccountLong(cmhead_misc_accnt_id),"
+         "       NULL, NULL, cmhead_misc,"
+         "       cmhead_misc,"
+         "       99999, 1 AS xtindentrole, 4 AS seq "
+         "FROM cmhead LEFT OUTER JOIN"
+         "     accnt ON (accnt_id=findFreightAccount(cmhead_cust_id)) "
+         "WHERE ((NOT cmhead_posted)"
+         "   AND (NOT cmhead_hold)"
+         "   AND (cmhead_misc <> 0))"
+
+         "UNION "
+         "SELECT cmhead_id, -1,"
+         "       NULL, 'Debit',"
+         "       CASE WHEN (tax_sales_accnt_id IS NULL) THEN 'Taxes'"
+         "            ELSE formatGLAccountLong(tax_sales_accnt_id)"
+         "       END,"
+         "       NULL, NULL, cmhead_tax,"
+         "       cmhead_tax,"
+         "       99999, 1 AS xtindentrole, 5 AS seq "
+         "FROM cmhead LEFT OUTER JOIN"
+         "     tax ON (cmhead_tax_id=tax_id)"
+         "WHERE ((NOT cmhead_posted)"
+         "   AND (NOT cmhead_hold)"
+         "   AND (cmhead_tax <> 0))"
+
+         "UNION "
+         "SELECT cmhead_id, -1,"
+         "       NULL, 'Credit',"
+         "       CASE WHEN (accnt_id IS NULL) THEN 'Not Assigned'"
+         "            ELSE formatGLAccountLong(accnt_id)"
+         "       END,"
+         "       NULL, NULL, NULL,"
+         "       SUM((cmitem_qtycredit * cmitem_qty_invuomratio) *"
+         "                  (cmitem_unitprice / cmitem_price_invuomratio)) +"
+         "           cmhead_freight + cmhead_misc + cmhead_tax AS extprice,"
+         "       99999, 1 AS xtindentrole, 6 AS seq "
+         "FROM cmhead LEFT OUTER JOIN"
+         "     cmitem ON (cmhead_id=cmitem_cmhead_id) LEFT OUTER JOIN"
+         "     accnt ON (accnt_id=findARAccount(cmhead_cust_id))"
+         "WHERE ((NOT cmhead_posted)"
+         "  AND  (NOT cmhead_hold)) "
+         "GROUP BY cmhead_id, cmhead_cust_id, cmhead_freight, cmhead_misc,"
+         "         cmhead_tax, accnt_id "
+         ") AS dummy "
+         "ORDER BY docnumber, cmitem_linenumber, seq;"
+        );
+  _cmhead->populate(q, true);
+  if (q.lastError().type() != QSqlError::None)
   {
-    int           thisOrderid;
-    int           orderid    = -9999;
-    XTreeWidgetItem *orderLine = NULL;
-    XTreeWidgetItem *lastLine  = NULL;
-    XTreeWidgetItem *selected  = NULL;
-
-    //  Fill the list with the query contents
-    do
-    {
-      thisOrderid = q.value("orderid").toInt();
-
-//  Check to see if this a new order number
-      if (thisOrderid != orderid)
-      {
-//  New order number, make a new list item header
-        orderid  = thisOrderid;
-        lastLine = NULL;
-
-        XTreeWidgetItem *thisLine = new XTreeWidgetItem( _cmhead, orderLine, q.value("orderid").toInt(), -1,
-                                                     q.value("documentnumber"), q.value("ordernumber"),
-                                                     q.value("cust_number"), q.value("billtoname") );
-        orderLine = thisLine;
-
-//  Add the distribution line
-        thisLine = new XTreeWidgetItem( orderLine, q.value("orderid").toInt(), -1,
-                                      "", "",
-                                      q.value("sence"), q.value("account"),
-                                      "", "", "", q.value("extprice") );
-        if (q.value("account") == "Not Assigned")
-        {
-          thisLine->setTextColor("red");
-          orderLine->setTextColor("red");
-        }
-
-//  If we are looking for a selected order and this is it, cache it
-        if (thisOrderid == _orderid)
-          selected = orderLine;
-      }
-      else
-      {
-        XTreeWidgetItem *itemLine = new XTreeWidgetItem( orderLine, lastLine, q.value("orderid").toInt(), q.value("itemid").toInt(),
-                                                     "", "",
-                                                     q.value("item"), q.value("itemdescrip"),
-                                                     q.value("iteminvuom"), q.value("qtytobill"),
-                                                     q.value("price"), q.value("extprice") );
-
-//  Add the distribution line
-        XTreeWidgetItem *thisLine = new XTreeWidgetItem( itemLine, q.value("orderid").toInt(), -1,
-                                                     "", "",
-                                                     q.value("sence"), q.value("account"),
-                                                     "", "", "", q.value("extprice") );
-        if (q.value("account") == "Not Assigned")
-        {
-          thisLine->setTextColor("red");
-          itemLine->setTextColor("red");
-          orderLine->setTextColor("red");
-        }
-
-        lastLine = itemLine;
-      }
-    }
-    while (q.next());
-
-//  Select and show the select item, if any
-    if (selected != NULL)
-    {
-      _cmhead->setCurrentItem(selected);
-      _cmhead->scrollToItem(selected);
-    }
+    systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+    return;
   }
 }
 
