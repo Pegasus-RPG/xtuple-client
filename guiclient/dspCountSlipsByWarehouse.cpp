949d99fe6c797668a5333d4ec36316b74462c0ef
gilmoskowitz <gilmoskowitz>
2008-09-30 12:48:14 +0000

    bug #3146, bug #6256 - xtreewidget localization
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@3621 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/dspCountSlipsByWarehouse.cpp
+++ guiclient/dspCountSlipsByWarehouse.cpp
@@ -1,57 +1,57 @@
 /*
- * Common Public Attribution License Version 1.0. 
- * 
- * The contents of this file are subject to the Common Public Attribution 
- * License Version 1.0 (the "License"); you may not use this file except 
- * in compliance with the License. You may obtain a copy of the License 
- * at http://www.xTuple.com/CPAL.  The License is based on the Mozilla 
- * Public License Version 1.1 but Sections 14 and 15 have been added to 
- * cover use of software over a computer network and provide for limited 
- * attribution for the Original Developer. In addition, Exhibit A has 
+ * Common Public Attribution License Version 1.0.
+ *
+ * The contents of this file are subject to the Common Public Attribution
+ * License Version 1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License
+ * at http://www.xTuple.com/CPAL.  The License is based on the Mozilla
+ * Public License Version 1.1 but Sections 14 and 15 have been added to
+ * cover use of software over a computer network and provide for limited
+ * attribution for the Original Developer. In addition, Exhibit A has
  * been modified to be consistent with Exhibit B.
- * 
- * Software distributed under the License is distributed on an "AS IS" 
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See 
- * the License for the specific language governing rights and limitations 
- * under the License. 
- * 
- * The Original Code is xTuple ERP: PostBooks Edition 
- * 
- * The Original Developer is not the Initial Developer and is __________. 
- * If left blank, the Original Developer is the Initial Developer. 
- * The Initial Developer of the Original Code is OpenMFG, LLC, 
- * d/b/a xTuple. All portions of the code written by xTuple are Copyright 
- * (c) 1999-2008 OpenMFG, LLC, d/b/a xTuple. All Rights Reserved. 
- * 
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is xTuple ERP: PostBooks Edition
+ *
+ * The Original Developer is not the Initial Developer and is __________.
+ * If left blank, the Original Developer is the Initial Developer.
+ * The Initial Developer of the Original Code is OpenMFG, LLC,
+ * d/b/a xTuple. All portions of the code written by xTuple are Copyright
+ * (c) 1999-2008 OpenMFG, LLC, d/b/a xTuple. All Rights Reserved.
+ *
  * Contributor(s): ______________________.
- * 
- * Alternatively, the contents of this file may be used under the terms 
- * of the xTuple End-User License Agreeement (the xTuple License), in which 
- * case the provisions of the xTuple License are applicable instead of 
- * those above.  If you wish to allow use of your version of this file only 
- * under the terms of the xTuple License and not to allow others to use 
- * your version of this file under the CPAL, indicate your decision by 
- * deleting the provisions above and replace them with the notice and other 
- * provisions required by the xTuple License. If you do not delete the 
- * provisions above, a recipient may use your version of this file under 
+ *
+ * Alternatively, the contents of this file may be used under the terms
+ * of the xTuple End-User License Agreeement (the xTuple License), in which
+ * case the provisions of the xTuple License are applicable instead of
+ * those above.  If you wish to allow use of your version of this file only
+ * under the terms of the xTuple License and not to allow others to use
+ * your version of this file under the CPAL, indicate your decision by
+ * deleting the provisions above and replace them with the notice and other
+ * provisions required by the xTuple License. If you do not delete the
+ * provisions above, a recipient may use your version of this file under
  * either the CPAL or the xTuple License.
- * 
+ *
  * EXHIBIT B.  Attribution Information
- * 
- * Attribution Copyright Notice: 
+ *
+ * Attribution Copyright Notice:
  * Copyright (c) 1999-2008 by OpenMFG, LLC, d/b/a xTuple
- * 
- * Attribution Phrase: 
+ *
+ * Attribution Phrase:
  * Powered by xTuple ERP: PostBooks Edition
- * 
- * Attribution URL: www.xtuple.org 
+ *
+ * Attribution URL: www.xtuple.org
  * (to be included in the "Community" menu of the application if possible)
- * 
- * Graphic Image as provided in the Covered Code, if any. 
+ *
+ * Graphic Image as provided in the Covered Code, if any.
  * (online at www.xtuple.com/poweredby)
- * 
- * Display of Attribution Information is required in Larger Works which 
- * are defined in the CPAL as a work which combines Covered Code or 
+ *
+ * Display of Attribution Information is required in Larger Works which
+ * are defined in the CPAL as a work which combines Covered Code or
  * portions thereof with code not governed by the terms of the CPAL.
  */
 
@@ -59,17 +59,24 @@
 
 #include <QMenu>
 #include <QMessageBox>
+#include <QSqlError>
 #include <QVariant>
 
 #include <openreports.h>
 #include <parameter.h>
+
+#define DEBUG true
 
 dspCountSlipsByWarehouse::dspCountSlipsByWarehouse(QWidget* parent, const char* name, Qt::WFlags fl)
     : XWidget(parent, name, fl)
 {
   setupUi(this);
 
-//  (void)statusBar();
+  // before connect so we don't repeatedly trigger the query
+  if (_preferences->boolean("XCheckBox/forgetful"))
+    _showUnposted->setChecked(true);
+  _dates->setStartNull(tr("Earliest"), omfgThis->startOfTime(), TRUE);
+  _dates->setEndNull(tr("Latest"), omfgThis->endOfTime(), TRUE);
 
   connect(_print, SIGNAL(clicked()), this, SLOT(sPrint()));
   connect(_cntslip, SIGNAL(populateMenu(QMenu*,QTreeWidgetItem*,int)), this, SLOT(sPopulateMenu(QMenu*,QTreeWidgetItem*)));
@@ -78,36 +85,24 @@ dspCountSlipsByWarehouse::dspCountSlipsByWarehouse(QWidget* parent, const char* 
   connect(_warehouse, SIGNAL(updated()), this, SLOT(sFillList()));
   connect(_dates, SIGNAL(updated()), this, SLOT(sFillList()));
 
-  _dates->setStartNull(tr("Earliest"), omfgThis->startOfTime(), TRUE);
-  _dates->setEndNull(tr("Latest"), omfgThis->endOfTime(), TRUE);
+  _cntslip->addColumn(tr("Slip #"),     _itemColumn, Qt::AlignLeft,  true, "slipnumber");
+  _cntslip->addColumn(tr("Tag #"),     _orderColumn, Qt::AlignLeft,  true, "invcnt_tagnumber");
+  _cntslip->addColumn(tr("Site"),        _whsColumn, Qt::AlignCenter,true, "warehous_code");
+  _cntslip->addColumn(tr("Item"),       _itemColumn, Qt::AlignLeft,  true, "item_number");
+  _cntslip->addColumn(tr("Description"),         -1, Qt::AlignLeft,  true, "descrip");
+  _cntslip->addColumn(tr("Entered"),    _dateColumn, Qt::AlignCenter,true, "cntslip_entered");
+  _cntslip->addColumn(tr("By"),         _userColumn, Qt::AlignCenter,true, "user");
+  _cntslip->addColumn(tr("Qty. Counted"),_qtyColumn, Qt::AlignRight, true, "cntslip_qty");
+  _cntslip->addColumn(tr("Posted"),     _dateColumn, Qt::AlignCenter,true, "cntslip_posted");
 
-  _cntslip->addColumn(tr("Slip #"),       _itemColumn,  Qt::AlignLeft   );
-  _cntslip->addColumn(tr("Tag #"),        _orderColumn, Qt::AlignLeft   );
-  _cntslip->addColumn(tr("Site"),         _whsColumn,   Qt::AlignCenter );
-  _cntslip->addColumn(tr("Item"),         _itemColumn,  Qt::AlignLeft   );
-  _cntslip->addColumn(tr("Description"),  -1,           Qt::AlignLeft   );
-  _cntslip->addColumn(tr("Entered (By)"), _userColumn,  Qt::AlignCenter );
-  _cntslip->addColumn(tr("Qty. Counted"), _qtyColumn,   Qt::AlignRight  );
-  _cntslip->addColumn(tr("Posted"),       _dateColumn,  Qt::AlignCenter );
-
-  if (_preferences->boolean("XCheckBox/forgetful"))
-    _showUnposted->setChecked(true);
-  
   sFillList();
 }
 
-/*
- *  Destroys the object and frees any allocated resources
- */
 dspCountSlipsByWarehouse::~dspCountSlipsByWarehouse()
 {
   // no need to delete child widgets, Qt does it all for us
 }
 
-/*
- *  Sets the strings of the subwidgets using the current
- *  language.
- */
 void dspCountSlipsByWarehouse::languageChange()
 {
   retranslateUi(this);
@@ -138,21 +133,19 @@ void dspCountSlipsByWarehouse::sPopulateMenu(QMenu *, QTreeWidgetItem *)
 
 void dspCountSlipsByWarehouse::sFillList()
 {
-  _cntslip->clear();
-
   QString sql("SELECT cntslip_id, ");
 
   if (_numericSlips->isChecked())
-    sql += "CASE WHEN (isNumeric(cntslip_number)) THEN TEXT(toNumeric(cntslip_number, 0))"
-           "     ELSE cntslip_number "
-           "END AS slipnumber, ";
+    sql += "toNumeric(cntslip_number, 0) AS slipnumber,"
+           "cntslip_number AS slipnumber_qtdisplayrole," ;
   else
     sql += "cntslip_number AS slipnumber, ";
 
   sql += " invcnt_tagnumber, warehous_code,"
-         " item_number, (item_descrip1 || ' ' || item_descrip2) AS description,"
-         " (formatDate(cntslip_entered) || ' (' || getUsername(cntslip_user_id) || ')') AS f_entered, "
-         " formatQty(cntslip_qty) AS f_qty, formatBoolYN(cntslip_posted) AS posted "
+         " item_number, (item_descrip1 || ' ' || item_descrip2) AS descrip,"
+         " cntslip_entered, getUsername(cntslip_user_id) AS user,"
+         " cntslip_qty, 'qty' AS cntslip_qty_xtnumericrole,"
+         " cntslip_posted "
          "FROM cntslip, invcnt, itemsite, item, warehous "
          "WHERE ((cntslip_cnttag_id=invcnt_id)"
          " AND (invcnt_itemsite_id=itemsite_id)"
@@ -167,46 +160,55 @@ void dspCountSlipsByWarehouse::sFillList()
     sql += " AND (itemsite_warehous_id=:warehous_id)";
 
   sql += ") "
-         "ORDER BY slipnumber";
+         "ORDER BY cntslip_number";
+
+  if (DEBUG)
+    qDebug("dspCountSlipsByWarehouse::sFillList() about to populate _cntslip");
 
   q.prepare(sql);
   _dates->bindValue(q);
   _warehouse->bindValue(q);
   q.exec();
-  if (q.first())
+  _cntslip->populate(q);
+  if (q.lastError().type() != QSqlError::None)
   {
-    int           slipNumber = -1;
-    XTreeWidgetItem *last      = NULL;
+    systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+    return;
+  }
 
-    do
+  if (_numericSlips->isChecked() && _cntslip->topLevelItemCount() > 1)
+  {
+    if (DEBUG)
+      qDebug("dspCountSlipsByWarehouse::sFillList() looking for slip # gaps "
+             "in %d items", _cntslip->topLevelItemCount());
+    XTreeWidgetItem *last =_cntslip->topLevelItem(0);
+    int slipNumber = last->rawValue("slipnumber").toInt();
+    for (int i = 1; i < _cntslip->topLevelItemCount(); i++)
     {
-      if (_numericSlips->isChecked())
+      XTreeWidgetItem *curr = _cntslip->topLevelItem(i);
+      if (DEBUG)
+        qDebug("row %d has slipNumber %d and current %d",
+               i, slipNumber, curr->rawValue("slipnumber").toInt());
+      if (slipNumber == (curr->rawValue("slipnumber").toInt() - 1) || slipNumber == -1)
+        slipNumber = curr->rawValue("slipnumber").toInt();
+      else if (slipNumber >= 0)
       {
-        if ( (slipNumber != -1) && (slipNumber != (q.value("slipnumber").toInt() - 1)) )
-        {
-          if (slipNumber == q.value("slipnumber").toInt() - 2)
-            last = new XTreeWidgetItem( _cntslip, last, -1,
-                                      QVariant("----"), "----", "----", "----",
-                                      tr("Missing Slip #%1").arg(slipNumber + 1),
-                                      "----", "----", "----" );
-          else
-            last = new XTreeWidgetItem( _cntslip, last, -1,
-                                      QVariant("----"), "----", "----", "----",
-                                      tr("Missing Slips #%1 to #%2").arg(slipNumber + 1).arg(q.value("slipnumber").toInt() - 1),
-                                      "----", "----", "----" );
+        if (slipNumber == curr->rawValue("slipnumber").toInt() - 2)
+          curr = new XTreeWidgetItem( _cntslip, last, -1,
+                                    QVariant("----"), "----", "----", "----",
+                                    tr("Missing Slip #%1").arg(slipNumber + 1),
+                                    "----", "----", "----" );
+        else
+          curr = new XTreeWidgetItem( _cntslip, last, -1,
+                                    QVariant("----"), "----", "----", "----",
+                                    tr("Missing Slips #%1 to #%2").arg(slipNumber + 1).arg(curr->rawValue("slipnumber").toInt() - 1),
+                                    "----", "----", "----" );
 
-          last->setTextColor("red");
-        }
-
-        slipNumber = q.value("slipnumber").toInt();
+        curr->setTextColor(namedColor("error"));
+        slipNumber = -1;
+        //i++; // 'cause we just added an item!
       }
-
-      last = new XTreeWidgetItem( _cntslip, last, q.value("cntslip_id").toInt(),
-                                q.value("slipnumber"), q.value("invcnt_tagnumber").toString(),
-                                q.value("warehous_code").toString(), q.value("item_number").toString(),
-                                q.value("description").toString(), q.value("f_entered").toString(),
-                                q.value("f_qty").toString(), q.value("posted").toString() );
+      last = curr;
     }
-    while (q.next());
   }
 }
