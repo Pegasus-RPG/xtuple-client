66b8a1e537b2b56c6639891e138c2edd26d91676
sunsetsolutions <sunsetsolutions>
2008-12-03 17:31:08 +0000

    Issue #7866:delete associated ipsfreight when deleting ipshead
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@4377 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/itemPricingSchedules.cpp
+++ guiclient/itemPricingSchedules.cpp
@@ -224,6 +224,8 @@ void itemPricingSchedules::sDelete()
              "WHERE (ipsitem_ipshead_id=:ipshead_id); "
              "DELETE FROM ipsprodcat "
              "WHERE (ipsprodcat_ipshead_id=:ipshead_id); "
+             "DELETE FROM ipsfreight "
+             "WHERE (ipsfreight_ipshead_id=:ipshead_id); "
              "DELETE FROM ipshead "
              "WHERE (ipshead_id=:ipshead_id);" );
   q.bindValue(":ipshead_id", _ipshead->id());
