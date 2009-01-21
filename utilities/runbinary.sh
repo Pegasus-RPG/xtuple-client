a3d88aeb2982defb2f20af88bd3e62a5628c12cb
cryan <cryan>
2009-01-21 22:42:35 +0000

    Updated/Added license file header information. New header is shorter with url reference to full license. UI files now have comments containing same brief license content. Updated Copyright year information.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@5078 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- utilities/runbinary.sh
+++ utilities/runbinary.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+# This file is part of the xTuple ERP: PostBooks Edition, a free and
+# open source Enterprise Resource Planning software suite,
+# Copyright (c) 1999-2009 by OpenMFG LLC, d/b/a xTuple.
+# It is licensed to you under the Common Public Attribution License
+# version 1.0, the full text of which (including xTuple-specific Exhibits)
+# is available at www.xtuple.com/CPAL.  By using this software, you agree
+# to be bound by its terms.
+#
 
 # Get the path to this program. All the libraries and
 # executables we need should be in the same location.
