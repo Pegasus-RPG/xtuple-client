1d402d480361bfa937035cd9d85053853003a3f2
cryan <cryan>
2009-01-28 15:16:32 +0000

    Fixed the name function to find the first class element and not assume the first element is always class.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@5106 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/xTupleDesigner.cpp
+++ guiclient/xTupleDesigner.cpp
@@ -250,7 +250,7 @@ QString xTupleDesigner::name()
   QDomDocument xmldoc("UIFile");
   _source->reset();
   xmldoc.setContent(_source);
-  QDomNode classnode = xmldoc.firstChild().firstChild();
+  QDomNode classnode = xmldoc.firstChild().firstChildElement("class");
   if (classnode.isNull())
   {
     qWarning("xTupleDesigner::name() classnode is null");
