12991197f8482e975fa131c63ae4e64ba26204bb
jrogelstad <jrogelstad>
2009-03-31 23:31:30 +0000

    More progress on simple sales order example.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@5527 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- share/sample_scripts/simpleSalesOrder/dbscripts/scripts/openSalesOrdersBySalesrep.js
+++ share/sample_scripts/simpleSalesOrder/dbscripts/scripts/openSalesOrdersBySalesrep.js
@@ -1,29 +1,39 @@
 // Define Variables
-var _all		= mywindow.findChild("_all");
-var _close 		= mywindow.findChild("_close");
-var _edit 		= mywindow.findChild("_edit");
-var _print 		= mywindow.findChild("_print");
-var _orders		= mywindow.findChild("_orders");
+var _all	= mywindow.findChild("_all");
+var _close 	= mywindow.findChild("_close");
+var _edit 	= mywindow.findChild("_edit");
+var _new	= mywindow.findChild("_new");
+var _print 	= mywindow.findChild("_print");
+var _orders	= mywindow.findChild("_orders");
 var _salesrep	= mywindow.findChild("_salesrep");
 var _selected	= mywindow.findChild("_selected");
-var _view  		= mywindow.findChild("_view");
+var _view  	= mywindow.findChild("_view");
+
+var _newMode 	= 0;
+var _editMode 	= 1;
+var _viewMode	= 2;
 
 // Set up columns
 with (_orders)
 {
-  addColumn("Order#",               75, 1, true, "cohead_number");
-  addColumn("Customer#",            75, 1, true, "cust_number");
-  addColumn("Name",                 -1, 1, true, "cust_name");
-  addColumn("P/O#", 		  75, 1, true, "cohead_custponumber");
-  addColumn("Sched. Date",          75, 1, true, "scheddate");
+  addColumn("Order#",		75, 1, true, "cohead_number");
+  addColumn("Customer#",	75, 1, true, "cust_number");
+  addColumn("Name",		-1, 1, true, "cust_name");
+  addColumn("P/O#",		75, 1, true, "cohead_custponumber");
+  addColumn("Sched. Date",	75, 1, true, "scheddate");
 }
 
 // Make connections
-_all["toggled(bool)"].connect(fillList);
 _close.clicked.connect(mywindow, "close");
+
+_all["toggled(bool)"].connect(fillList);
 _salesrep["newID(int)"].connect(fillList);
 
-// Execute query
+_new.clicked.connect(orderNew);
+_edit.clicked.connect(orderEdit);
+_view.clicked.connect(orderView);
+
+// Populate
 fillList();
 
 function fillList()
@@ -34,11 +44,50 @@ function fillList()
     if (_selected.checked)
       params.salesrep_id = _salesrep.id();
 
-    data = toolbox.executeDbQuery("salesOrdersCustom","detail",params);
+    data = toolbox.executeDbQuery("salesorderscustom","detail",params);
     _orders.populate(data);
   }
   catch(e)
   {
     print(e);
+    toolbox.messageBox("critical", mywindow, mywindow.windowTitle, e);
   }
+}
+
+function orderEdit()
+{
+  orderOpen(_editMode,_orders.id());
+}
+
+function orderNew()
+{
+  orderOpen(_newMode,0);
+}
+
+function orderOpen(mode,number)
+{
+  try
+  {
+    var childwnd = toolbox.openWindow("simpleSalesOrder", mywindow, 0, 1);
+    var params   = new Object;
+
+    params.mode   = mode;
+    if (mode) // Edit or View
+      params.filter = "order_number='" + number + "'";
+
+    var tmp = toolbox.lastWindow().set(params);
+    var execval = childwnd.exec();
+    if (execval)
+      fillList();
+  }
+  catch(e)
+  {
+    print(e);
+    toolbox.messageBox("critical", mywindow, mywindow.windowTitle, e);
+  }
+}
+
+function orderView()
+{
+  orderOpen(_viewMode,_orders.id());
 }
--- share/sample_scripts/simpleSalesOrder/dbscripts/scripts/simpleSalesOrder.js
+++ share/sample_scripts/simpleSalesOrder/dbscripts/scripts/simpleSalesOrder.js
@@ -1,43 +1,45 @@
-print("running");
 // Define local variables
 var _linenumCol = 1;
 var _itemCol    = 2;	
-var _qtyCol     = 3;
-var _priceCol   = 4;
-var _extendCol  = 5;
-var _taxCol     = 6;
-var _schedCol   = 7;
+var _qtyCol     = 7;
+var _priceCol   = 9;
 
 var _billAddrNo = "";
 var _linenumber = 1;
 var _populating = false;
+
+var _extList = new Array();
+var _taxList = new Array();
 
 var _newMode    = 0;
 var _editMode   = 1;
 var _viewMode   = 2;
 
 var _add		= mywindow.findChild("_add");
-var _address	= mywindow.findChild("_address");
+var _address		= mywindow.findChild("_address");
 var _cancel		= mywindow.findChild("_cancel");
-var _cust      	= mywindow.findChild("_cust");
-var _custTab	= mywindow.findChild("_custTab");
+var _cust      		= mywindow.findChild("_cust");
+var _custTab		= mywindow.findChild("_custTab");
 var _extendedPrice	= mywindow.findChild("_extendedPrice");
-var _item      	= mywindow.findChild("_item");
+var _item		= mywindow.findChild("_item");
 var _itemGroup    	= mywindow.findChild("_itemGroup");
-var _itemsTab          = mywindow.findChild("_itemsTab");
+var _itemsTab		= mywindow.findChild("_itemsTab");
 var _number		= mywindow.findChild("_number");
-var _qty       	= mywindow.findChild("_qty");
+var _qty		= mywindow.findChild("_qty");
 var _remove		= mywindow.findChild("_remove");
 var _sale		= mywindow.findChild("_sale");
-var _saleitem 	= mywindow.findChild("_saleitem");
-var _saleitems 	= mywindow.findChild("_saleitems");
-var _salesrep	= mywindow.findChild("_salesrep");
+var _saleitem		= mywindow.findChild("_saleitem");
+var _saleitems		= mywindow.findChild("_saleitems");
+var _salesrep		= mywindow.findChild("_salesrep");
 var _save		= mywindow.findChild("_save");
+var _scheddate		= mywindow.findChild("_scheddate");
 var _shipto		= mywindow.findChild("_shipto");
-var _shipvia	= mywindow.findChild("_shipvia")
+var _shipvia		= mywindow.findChild("_shipvia")
+var _site		= mywindow.findChild("_site");
 var _subtotal   	= mywindow.findChild("_subtotal");
 var _tab		= mywindow.findChild("_tab");
 var _tax		= mywindow.findChild("_tax");
+var _taxauth		= mywindow.findChild("_taxauth");
 var _total		= mywindow.findChild("_total");	
 var _unitPrice  	= mywindow.findChild("_unitPrice");
 
@@ -45,19 +47,18 @@ var _unitPrice  	= mywindow.findChild("_unitPrice");
 with (_saleitems)
 {
   setColumn("Line#"  		, 40,  0, true,  "line_number");
-  setColumn("Item"   		, 100, 0, true,  "item_number");
+  setColumn("Item"   		, -1,  0, true,  "item_number");
   setColumn("Customer P/N"   	, 100, 0, false, "customer_pn");
   setColumn("Substitute for"   	, 100, 0, false, "substitute_for");
   setColumn("Site"   		, 40,  0, false, "sold_from_site");
-  setColumn("Status"   	, 40,  0, false, "status");
-  setColumn("Quantity"		, 80 , 0, true,  "qty_ordered");
+  setColumn("Status"   		, 40,  0, false, "status");
+  setColumn("Quantity"		, 100, 0, true,  "qty_ordered");
   setColumn("Qty UOM"   	, 40,  0, false, "qty_uom");
-  setColumn("Price" 		, 60 , 0, true,  "net_unit_price");
+  setColumn("Price" 		, 100, 0, true,  "net_unit_price");
   setColumn("Price UOM"   	, 40,  0, false, "price_uom");
   setColumn("Extended"		, 100, 0, true,  "extension");
-  setColumn("Tax"      	, 60 , 0, false, "tax");
-  setColumn("Sched. Date"	, 80 , 0, true,  "scheduled_date");
-  setColumn("Promise Date"	, 80 , 0, false, "promise_date");
+  setColumn("Sched. Date"	, 100, 0, true,  "scheduled_date");
+  setColumn("Promise Date"	, 80,  0, false, "promise_date");
   setColumn("Warranty"   	, 40,  0, false, "warranty");
   setColumn("Tax Type"   	, 80,  0, false, "tax_type");
   setColumn("Tax Code"   	, 80,  0, false, "tax_code");
@@ -70,7 +71,7 @@ with (_saleitems)
 
 // Define connections
 _add.clicked.connect(add);
-_cancel.clicked.connect(mydialog, "reject");
+_cancel.clicked.connect(cancel);
 _cust["newId(int)"].connect(handleButtons);
 _cust["newId(int)"].connect(populateCustomer);
 _item["newId(int)"].connect(handleButtons);
@@ -106,52 +107,56 @@ function add()
   _item.setFocus();
 }
 
+function cancel()
+{
+  if (_sale.mode == _newMode)
+  {
+    params = new Object;
+    params.number = _number.text;
+    toolbox.executeDbQuery("simplesalesorder","releasesonumber",params);
+  }
+  
+  mydialog.reject();
+}
+
 function extension()
 {
-  if (_populating)  // No need to keep recalculating now
-    return;
+  var ext = _qty.text * _unitPrice.localValue;
+  _extendedPrice.setLocalValue(ext)
 
-  var extCache = _extendedPrice.localValue;
-  var tax;
-  var taxCache = _saleitems.selectedValue(_taxCol);
-  var row  = 0;
-  var hsub = 0;
-  var htax = 0;
+  if (_populating)  // No need to keep recalculating totals
+    return;
 
   try
   {
+    var row = _saleitem.currentIndex();
+    var tax;
+
+    // Remove original values from total
+    _subtotal.localValue = _subtotal.localValue -_extList[row];
+    _tax.localValue = _tax.localValue -_taxList[row];
+
     // Update item extension
-    var ext = _qty.text * _unitPrice.localValue;
-    _extendedPrice.setLocalValue(ext);
+    _extList[row] = ext;
+    _subtotal.localValue = _subtotal.localValue + ext;
 
-    // Recalculate Tax
+    // Recalculate tax
     var params = new Object;
-    params.item_id    = _item.id();
-    params.qty        = _qty.text;
-    params.unit_price = _unitPrice.localValue;
+    params.taxauth_id = _taxauth.id();
+    params.item_number = _item.itemNumber();
+    params.qty = _qty.text - 0;
+    params.unit_price = _unitPrice.localValue - 0;
   
-    var data = toolbox.executeDbQuery("sale","saleitemtax",params);
+    var data = toolbox.executeDbQuery("simplesalesorder","itemtax",params);
     if (data.first())
-    { 
       tax = data.value("item_tax") - 0;
-      _saleitems.setValue(_saleitem.currentIndex(),_taxCol,tax);
-    }
+    else
+      tax = 0 - 0;
+    _taxList[row] = tax;
+    _tax.localValue = _tax.localValue + tax;
 
-    // Update header info
-    while (row < _saleitems.rowCount())
-    {
-      if (!_saleitems.isRowHidden(row))
-      {
-        hsub += _saleitems.value(row, _qtyCol) * 		
-		_saleitems.value(row, _priceCol);
-        htax += _saleitems.value(row, _taxCol) - 0;
-      }
-      row++;
-    }
-
-    _subtotal.localValue = hsub;
-    _tax.localValue = htax;
-    _total.localValue = hsub + htax;
+    // Update total
+    _total.localValue = _subtotal.localValue + _tax.localValue;
   }
   catch (e)
   {
@@ -169,10 +174,11 @@ function handleItem()
                + "FROM item "
                + "   JOIN uom      ON (item_inv_uom_id=uom_id)"
                + "   JOIN itemsite ON (item_id=itemsite_item_id) "
-               + "WHERE ((item_active"
-               + "   AND item_sold) "
-               + "   AND itemsite_sold)"
-               + "   AND itemsite_active));");
+               + "WHERE ((itemsite_warehous_id=" + _site.id() + ")"
+               + "   AND (item_active)"
+               + "   AND (item_sold) "
+               + "   AND (itemsite_sold)"
+               + "   AND (itemsite_active));");
 }
 
 function handleButtons()
@@ -182,6 +188,8 @@ function handleButtons()
               (_saleitems.rowCountVisible() > 1 ||
                _item.number.length))
   _save.enabled = (state);
+  _site.enabled = (!state);
+  _taxauth.enabled = (!state);
 }
 
 function itemPrice()
@@ -198,7 +206,7 @@ function itemPrice()
   {
     if (_qty.text && _item.id() > 0)
     {
-      var data = toolbox.executeDbQuery("simpleSalesOrder","itemprice",params);
+      var data = toolbox.executeDbQuery("simplesalesorder","itemprice",params);
       if (data.first())
         _unitPrice.setLocalValue(data.value("itemprice"));
       else if (data.lastError())
@@ -229,49 +237,50 @@ function populate()
 
   _populating = false;
   handleButtons();
+  _cust.enabled = false;
 }
 
 function populateCustomer()
 {
+  if (_populating)
+    return;
+
   var params = new Object;
   params.cust_id = _cust.id();
 
-  var data = toolbox.executeDbQuery("sale","fetchcustomer", params);
+  var data = toolbox.executeDbQuery("simplesalesorder","fetchcustomer", params);
   if(data.first())
   {
     _salesrep.setId(data.value("cust_salesrep_id"));
     _billAddrNo = data.value("billto_addr_number");
     _address.setNumber(data.value("shipto_addr_number"));
+    _taxauth.setId(data.value("cust_taxauth"));
     _shipvia.text = (data.value("cust_shipvia"));
+    _site.setId(data.value("cust_preferred_warehous_id"));
   }
   else
   {
     _salesrep.setId(-1);
     _billAddrNo = "";
     _address.setNumber("");
-    _shipvia.text = ""
+    _taxauth.setId(-1);
+    _shipvia.text = "";
+    _site.setId(-1);
   }
 }
 
 function populateItems()
 {
-  try
-  {
-    _saleitems.populate(_sale.currentIndex());
-    _itemGroup.enabled = (_saleitems.rowCount());
-    _linenumber = _saleitems.rowCount() + 1;
-  }
-  catch(e)
-  {
-    print(e);
-    toolbox.messageBox("critical", mywindow, mywindow.windowTitle, e);
-  }
+  _saleitems.populate(_sale.currentIndex());
+  _itemGroup.enabled = (_saleitems.rowCount());
+  _linenumber = _saleitems.rowCount() + 1;
+  recalcTotals();
 }
 
 function prepare()
 {
   // Set header data
-  var data = toolbox.executeDbQuery("simpleSalesOrder", "fetchsonumber");
+  var data = toolbox.executeDbQuery("simplesalesorder", "fetchsonumber");
   if(data.first())
     _number.text = data.value("number");
 
@@ -281,6 +290,38 @@ function prepare()
   _add.enabled = false;
   _tab.setCurrentIndex(0);
   _cust.setFocus();
+}
+
+function recalcTotals()
+{
+  var params = new Object;
+  var data;
+  var ext = 0-0;
+  var tax = 0-0;
+  _subtotal.clear();
+  _tax.clear();
+  _total.clear();
+
+  for (var i = 0 - 0; i < _saleitems.rowCount(); i++)
+  {
+    ext = _saleitems.value(i,_qtyCol) * 
+          _saleitems.value(i,_priceCol);
+    _extList[i] = ext;
+    _subtotal.localValue = _subtotal.localValue + ext;
+
+    params.taxauth_id = _taxauth.id();
+    params.item_number = _saleitems.value(i,_itemCol);
+    params.qty = _saleitems.value(i,_qtyCol) - 0;
+    params.unit_price = _saleitems.value(i,_priceCol) - 0;
+    data = toolbox.executeDbQuery("simplesalesorder","itemtax",params);
+    if (data.first())
+      tax = data.value("item_tax") - 0;
+    else
+      tax = 0 - 0;
+    _taxList[i] = tax;
+    _tax.localValue = _tax.localValue + tax;
+  }
+  _total.localValue = _subtotal.localValue + _tax.localValue;
 }
 
 function remove()
@@ -339,13 +380,15 @@ function rowSelected(row)
       toolbox.messageBox("critical", mywindow, mywindow.windowTitle, msg);
       return;
     }
-    else if (_scheddate.text.length == 0)
+
+    else if (_scheddate.date.length == 0)
     {
       _saleitems.selectRow(currentRow);
       var msg = "You must enter a valid scheduled date or remove the current line."
       toolbox.messageBox("critical", mywindow, mywindow.windowTitle, msg);
       return;
     }
+
     else if (_qty.text.length == 0)
     {
       _saleitems.selectRow(currentRow);
@@ -368,7 +411,7 @@ function save()
     remove();
 
   _populating = true;
-  _contact.check();
+  _address.check();
 
   try
   {
@@ -395,11 +438,8 @@ function save()
 
 function set(input)
 {
-try{
-print("setting");
   if ("mode" in input)
   {
-print("moding");
     _sale.setMode(input.mode);
 
     if (input.mode == _newMode)
@@ -419,16 +459,9 @@ print("moding");
 
   if ("filter" in input) // Use to populate an existing record
   {
-print("setting filter: " + input.filter);
     _sale.setFilter(input.filter);
     populate();
   }
 
   return 0;
-  }
-  catch(e)
-  {
-    print(e);
-    toolbox.messageBox("critical", mywindow, mywindow.windowTitle, e);
-  }
 }
