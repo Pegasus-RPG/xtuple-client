5ed51e49fcb9f981bf45e42ba6b915747381eb50
sunsetsolutions <sunsetsolutions>
2009-04-11 16:49:00 +0000

    Issue #8275:add logging and progress bar
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@5625 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/purgeInvoices.cpp
+++ guiclient/purgeInvoices.cpp
@@ -12,6 +12,8 @@
 
 #include <qvariant.h>
 #include <qmessagebox.h>
+#include <QFile>
+#include <QProgressDialog>
 
 /*
  *  Constructs a purgeInvoices as a child of 'parent', with the
@@ -64,14 +66,73 @@ void purgeInvoices::sPurge()
   }
 
   if ( QMessageBox::warning( this, tr("Delete Invoice Records"),
-                             tr( "You will not be able to re-print an Invoice if you delete that Invoice's Records.\n"
+                             tr( "This function will purge Invoices and all of the associated documents including:\n"
+                                 "Return Authorizations, Lot/Serial Registrations, Sales Orders, Shipments, and Billing.\n"
+                                 "The results of the process are saved in the log file purgeInvoices.log.\n"
+                                 "You will not be able to re-print an Invoice if you delete that Invoice's Records.\n"
+                                 "\n"
                                  "Are you sure that you want to delete the selected Invoice Records?" ),
                              tr("Yes"), tr("No"), QString::null, 0, 1) == 0)
   {
-    q.prepare("SELECT purgeInvoiceRecords(:cutOffDate) AS result;");
-    q.bindValue(":cutOffDate", _cutOffDate->date());
-    q.exec();
+    
+    QString logpath;
+    QString logname;
+    QString logtogether;
+    logpath = "";
+    logname = "purgeInvoices.log";
+    logtogether = logpath + logname;
+    QFile logfile(logtogether);
+    logfile.remove();
+    logfile.open(QIODevice::WriteOnly);
 
+    int _invoices = 1;
+    int _kount = 1;
+    
+    XSqlQuery invoices;
+    invoices.prepare("SELECT COUNT(*) AS invoice_count "
+                     "FROM invchead "
+                     "WHERE ( (invchead_invcdate <= :cutOffDate) "
+                     "  AND   (invchead_posted) "
+                     "  AND   (NOT invchead_recurring) "
+                     "  AND   (checkInvoiceSitePrivs(invchead_id)) );");
+    invoices.bindValue(":cutOffDate", _cutOffDate->date());
+    invoices.exec();
+    if (invoices.first())
+      _invoices = (invoices.value("invoice_count").toInt());
+    
+    QProgressDialog _progress(tr("Purge Invoices in progress..."), tr("Cancel"), 0, _invoices, this);
+    _progress.setWindowModality(Qt::WindowModal); 
+            
+    invoices.prepare("SELECT invchead_id, invchead_invcnumber "
+                     "FROM invchead "
+                     "WHERE ( (invchead_invcdate <= :cutOffDate) "
+                     "  AND   (invchead_posted) "
+                     "  AND   (NOT invchead_recurring) "
+                     "  AND   (checkInvoiceSitePrivs(invchead_id)) );");
+    invoices.bindValue(":cutOffDate", _cutOffDate->date());
+    invoices.exec();
+    while (invoices.next())
+    {
+      q.prepare("SELECT purgeInvoiceRecord(:cutOffDate, :invchead_id) AS result;");
+      q.bindValue(":cutOffDate", _cutOffDate->date());
+      q.bindValue(":invchead_id", invoices.value("invchead_id").toInt());
+      q.exec();
+      if (q.first())
+      {
+        QString logmessage = "Invoice ";
+        logmessage += invoices.value("invchead_invcnumber").toString();
+        logmessage += ", result=";
+        logmessage += q.value("result").toString();
+        logfile.write(logmessage.toAscii());
+        logfile.write("\n");
+      }
+      if (_progress.wasCanceled())
+        break;
+      _progress.setValue(_kount);
+      _kount++;
+    }
+    _progress.setValue(_invoices);
+    
     accept();
   }
 }
