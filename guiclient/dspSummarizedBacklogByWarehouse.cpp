b3bfb63e7e73f118f3544d1b0f1584c2b5639e99
nmonday <nmonday>
2009-06-12 19:15:53 +0000

    Issue# 8552 Overhaul core to use new XTreeWidget methodologies.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@6141 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/dspSummarizedBacklogByWarehouse.cpp
+++ guiclient/dspSummarizedBacklogByWarehouse.cpp
@@ -77,20 +77,16 @@ void dspSummarizedBacklogByWarehouse::languageChange()
 
 void dspSummarizedBacklogByWarehouse::sHandlePrices(bool pShowPrices)
 {
-  if (pShowPrices)
-  {
-    _so->showColumn(6);
-    _so->showColumn(7);
-    _so->showColumn(8);
+  if(pShowPrices){
+    _so->showColumn("sales");
+    _so->showColumn("cost");
+    _so->showColumn("margin");
   }
-  else
-  {
-    _so->hideColumn(6);
-    _so->hideColumn(7);
-    _so->hideColumn(8);
+  else{
+    _so->hideColumn("sales");
+    _so->hideColumn("cost");
+    _so->hideColumn("margin");
   }
-
-  sFillList();
 }
 
 bool dspSummarizedBacklogByWarehouse::setParams(ParameterList &params)
@@ -103,15 +99,17 @@ bool dspSummarizedBacklogByWarehouse::setParams(ParameterList &params)
   else
     return false;
 
-  if (_showPrices->isChecked())
-    params.append("showPrices");
-
   if (_shipDate->isChecked())
     params.append("orderByShipDate");
   else if (_packDate->isChecked())
     params.append("orderByPackDate");
   else if (_orderNumber->isChecked())
     params.append("orderByOrderNumber");
+
+  if(_showPrices->isChecked())
+    params.append("showPrices");
+
+  sHandlePrices(_showPrices->isChecked());
 
   params.append("none",   tr("None"));
   params.append("credit", tr("Credit"));
@@ -283,154 +281,140 @@ void dspSummarizedBacklogByWarehouse::sPopulateMenu(QMenu *pMenu)
 
 void dspSummarizedBacklogByWarehouse::sFillList()
 {
-  _so->clear();
-
-  XTreeWidgetItem *orderLine  = NULL;
 
   ParameterList params;
   if (setParams(params))
   {
-    QString sql( "SELECT cohead_id, cohead_holdtype, cohead_number, cust_name,"
-	     "       cohead_created, cohead_orderdate, cohead_packdate, pack_head_id,"
-		 "       CASE WHEN (cohead_holdtype='N') THEN <? value(\"none\") ?>"
-		 "            WHEN (cohead_holdtype='C') THEN <? value(\"credit\") ?>"
-		 "            WHEN (cohead_holdtype='S') THEN <? value(\"ship\") ?>"
-		 "            WHEN (cohead_holdtype='P') THEN <? value(\"pack\") ?>"
-		 "            WHEN (cohead_holdtype='R') THEN <? value(\"return\") ?>"
-		 "            ELSE <? value(\"other\") ?>"
-		 "       END AS f_holdtype,"
-		 "       MIN(coitem_scheddate) AS scheddate,"
-		 "       SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) *"
-		 "            (currToBase(cohead_curr_id, coitem_price, cohead_orderdate) / coitem_price_invuomratio) ) AS sales,"
-		 "       SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) * stdcost(item_id) ) AS cost,"
-		 "       SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) *"
-		 "            ((currToBase(cohead_curr_id, coitem_price, cohead_orderdate) / coitem_price_invuomratio) - stdcost(item_id)) ) AS margin,"
-		 "       'curr' AS sales_xtnumericrole,"
-		 "       'curr' AS cost_xtnumericrole,"
-		 "       'curr' AS margin_xtnumericrole,"
-		 "       COALESCE(cosmisc_id, -1) AS cosmisc_id, "
-		 "       formatShipmentNumber(cosmisc_id) AS cosmisc_number, "
-		 "       CASE WHEN (cosmisc_shipped IS NULL) THEN 0"
-		 "            WHEN (cosmisc_shipped) THEN 1"
-		 "            WHEN (NOT cosmisc_shipped) THEN 2"
-		 "       END AS shipstatus,"
-		 "       COALESCE(cosmisc_shipvia, '') AS shipvia,"
-		 "       CASE WHEN (cosmisc_shipdate IS NULL) THEN ''"
-		 "            ELSE formatDate(cosmisc_shipdate)"
-		 "       END AS shipdate,"
-		 "       ( (COALESCE(cobmisc_cohead_id,0) > 0)"
-		 "       AND (SUM(noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned)) > 0)"
-		 "       ) AS overbilled,"
-		 "		 formatboolyn(CASE WHEN (pack_head_id IS NOT NULL) THEN TRUE"
-		 "            ELSE FALSE"
-		 "       END) AS packed "
-		 "FROM coitem, itemsite, item, cust, cohead "
-                 "  LEFT OUTER JOIN cosmisc ON (cosmisc_cohead_id=cohead_id) "
-                 "  LEFT OUTER JOIN (SELECT DISTINCT cobmisc_cohead_id FROM cobmisc) AS cobmisc ON (cobmisc_cohead_id=cohead_id) "
-				 "  LEFT OUTER JOIN pack ON (cohead_id = pack_head_id)"
-		 "WHERE ( (coitem_cohead_id=cohead_id)"
-		 " AND (cohead_cust_id=cust_id)"
-		 " AND (coitem_itemsite_id=itemsite_id)"
-		 " AND (itemsite_item_id=item_id)"
-		 " AND (coitem_status NOT IN ('C','X'))"
-		 " AND (coitem_scheddate BETWEEN <? value(\"startDate\") ?>"
-		 "                           AND <? value(\"endDate\") ?>)"
-		 "<? if exists(\"warehous_id\") ?>"
-		 " AND (itemsite_warehous_id=<? value(\"warehous_id\") ?>)"
-		 "<? endif ?>"
-		 "<? if exists(\"custtype_id\") ?>"
-		 " AND (cust_custtype_id=<? value(\"custtype_id\") ?>)"
-		 "<? elseif exists(\"custtype_pattern\") ?>"
-		 " AND (cust_custtype_id IN (SELECT custtype_id FROM custtype"
-		 "  WHERE (custtype_code ~ <? value(\"custtype_pattern\") ?>)))"
-		 "<? endif ?>"
-		 ") "
-		 "GROUP BY cohead_id, cohead_number, cust_name,"
-		 "         cohead_holdtype, cohead_orderdate, cohead_packdate,"
-		 "         cosmisc_shipped, cosmisc_shipvia, cosmisc_shipdate,"
-		 "         cosmisc_id, cobmisc_cohead_id, cohead_created, pack_head_id "
-		 " ORDER BY "
-		 "<? if exists(\"orderByShipDate\") ?>scheddate,"
-		 "<? elseif exists(\"orderByPackDate\") ?>cohead_packdate,"
-		 "<? endif ?>"
-		 "          cohead_number, cosmisc_shipped;");
+    QString sql( "(SELECT cohead_id, 0 AS xtindentrole, salesrep_name, cohead_holdtype, cohead_number, "
+   "CASE WHEN ((cosmisc_shipped) AND (COALESCE(cobmisc_cohead_id,0) > 0)       "
+   "AND (SUM(noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned)) <= 0)) THEN 'green'"
+   "WHEN ( (COALESCE(cobmisc_cohead_id,0) > 0)       "
+   "AND (SUM(noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned)) > 0)       "
+   ") THEN 'red' "
+   "WHEN (NOT cosmisc_shipped) THEN 'blue' "
+   "END AS qtforegroundrole, "
+   "cust_name,       "
+   "cohead_created, cohead_orderdate, cohead_packdate, pack_head_id,       "
+   "       CASE WHEN (cohead_holdtype='N') THEN <? value(\"Pnone\") ?>"
+   "            WHEN (cohead_holdtype='C') THEN <? value(\"Pcredit\") ?>"
+   "            WHEN (cohead_holdtype='S') THEN <? value(\"Pship\") ?>"
+   "            WHEN (cohead_holdtype='P') THEN <? value(\"Ppack\") ?>"
+   "            WHEN (cohead_holdtype='R') THEN <? value(\"Preturn\") ?>"
+   "            ELSE <? value(\"Pother\") ?>"
+   "END AS f_holdtype,       "
+   "MIN(coitem_scheddate) AS scheddate,       "
+   "SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) *            "
+   "            (currToBase(cohead_curr_id, coitem_price, cohead_orderdate) / coitem_price_invuomratio) ) AS sales, "
+   "       SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) * stdcost(item_id) ) AS cost,"
+   "       SUM((noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned) * coitem_qty_invuomratio) *"
+   "            ((currToBase(cohead_curr_id, coitem_price, cohead_orderdate) / coitem_price_invuomratio) - stdcost(item_id)) ) AS margin,"
+   "       'curr' AS sales_xtnumericrole,"
+   "       'curr' AS cost_xtnumericrole,"
+   "       'curr' AS margin_xtnumericrole,"
+   "       COALESCE(cosmisc_id, -1) AS cosmisc_id, "
+   "       formatShipmentNumber(cosmisc_id) AS cosmisc_number, "
+   "       CASE WHEN (cosmisc_shipped IS NULL) THEN 0"
+   "            WHEN (cosmisc_shipped) THEN 1"
+   "            WHEN (NOT cosmisc_shipped) THEN 2"
+   "       END AS shipstatus,"
+   "       COALESCE(cosmisc_shipvia, '') AS shipvia,"
+   "       CASE WHEN (cosmisc_shipdate IS NULL) THEN ''"
+   "            ELSE formatDate(cosmisc_shipdate)"
+   "       END AS shipdate,"
+   "		 formatboolyn(CASE WHEN (pack_head_id IS NOT NULL) THEN TRUE"
+   "            ELSE FALSE"
+   " END) AS packed "
+   "<? if exists(\"showPrices\") ?> "
+   " , 0 AS sales_xttotalrole, 0 AS cost_xttotalrole, 0 AS margin_xttotalrole "
+   "<? endif ?>"
+   "FROM coitem, itemsite, item, cust, cohead "
+   "  LEFT OUTER JOIN cosmisc ON (cosmisc_cohead_id=cohead_id) "
+   "  LEFT OUTER JOIN (SELECT DISTINCT cobmisc_cohead_id FROM cobmisc) AS cobmisc ON (cobmisc_cohead_id=cohead_id) "
+   "  LEFT OUTER JOIN pack ON (cohead_id = pack_head_id), salesrep "
+   "WHERE ( (coitem_cohead_id=cohead_id)"
+   " AND (salesrep_id = cohead_salesrep_id) "
+   " AND (cohead_cust_id=cust_id)"
+   " AND (coitem_itemsite_id=itemsite_id)"
+   " AND (itemsite_item_id=item_id)"
+   " AND (coitem_status NOT IN ('C','X'))"
+   " AND (coitem_scheddate BETWEEN <? value(\"startDate\") ?>"
+   "                           AND <? value(\"endDate\") ?>)"
+   "<? if exists(\"warehous_id\") ?>"
+   " AND (itemsite_warehous_id=<? value(\"warehous_id\") ?>)"
+   "<? endif ?>"
+   "<? if exists(\"custtype_id\") ?>"
+   " AND (cust_custtype_id=<? value(\"custtype_id\") ?>)"
+   "<? elseif exists(\"custtype_pattern\") ?>"
+   " AND (cust_custtype_id IN (SELECT custtype_id FROM custtype"
+   "  WHERE (custtype_code ~ <? value(\"custtype_pattern\") ?>)))"
+   "<? endif ?>"
+   ") "
+   "GROUP BY cohead_id, salesrep_number, salesrep_name, cohead_number, cust_name,"
+   "         cohead_holdtype, cohead_orderdate, cohead_packdate,"
+   "         cosmisc_shipped, cosmisc_shipvia, cosmisc_shipdate,"
+   "         cosmisc_id, cobmisc_cohead_id, cohead_created, pack_head_id "
+   " ORDER BY "
+   "<? if exists(\"orderByShipDate\") ?>scheddate,"
+   "<? elseif exists(\"orderByPackDate\") ?>cohead_packdate,"
+   "<? endif ?>"
+   "          cohead_number, cosmisc_shipped )"
+   "UNION "
+   "(SELECT cohead_id, 1 AS xtindentrole, NULL, NULL, "
+   "CASE WHEN (cosmisc_shipped IS NULL) THEN ''            "
+   "WHEN (cosmisc_shipped) THEN 'Yes'            "
+   "WHEN (NOT cosmisc_shipped) THEN 'No'       "
+   "END AS cohead_number, "
+   "CASE WHEN (cosmisc_shipped IS NULL) THEN ''           "
+   "WHEN (cosmisc_shipped) THEN 'green'            "
+   "WHEN (NOT cosmisc_shipped) THEN 'blue' "
+   "WHEN ( (COALESCE(cobmisc_cohead_id,0) > 0)       "
+   "AND (SUM(noNeg(coitem_qtyord - coitem_qtyshipped + coitem_qtyreturned)) > 0)       "
+   ") THEN 'red'       "
+   "END AS qtforegroundrole, "
+   "COALESCE(cosmisc_shipvia, ''), NULL, NULL, NULL, NULL, NULL,  MIN(coitem_scheddate), NULL, NULL, NULL, NULL, NULL, NULL, NULL,        "
+   "formatShipmentNumber(cosmisc_id) AS cosmisc_number,        "
+   "NULL,       "
+   "COALESCE(cosmisc_shipvia, '') AS shipvia,       "
+   "CASE WHEN (cosmisc_shipdate IS NULL) THEN ''            "
+   "ELSE formatDate(cosmisc_shipdate)       "
+   "END AS shipdate        "
+   "<? if exists(\"showPrices\") ?> "
+   " , NULL, NULL, NULL "
+   "<? endif ?>"
+   ", NULL "
+   "FROM coitem, itemsite, item, cust, cohead   "
+   "LEFT OUTER JOIN cosmisc ON (cosmisc_cohead_id=cohead_id)   "
+   "LEFT OUTER JOIN (SELECT DISTINCT cobmisc_cohead_id FROM cobmisc) AS cobmisc ON (cobmisc_cohead_id=cohead_id)   "
+   "LEFT OUTER JOIN pack ON (cohead_id = pack_head_id), salesrep "
+   "WHERE ( (coitem_cohead_id=cohead_id)"
+   " AND (salesrep_id = cohead_salesrep_id) "
+   " AND (cohead_cust_id=cust_id)"
+   " AND (coitem_itemsite_id=itemsite_id)"
+   " AND (itemsite_item_id=item_id)"
+   " AND (coitem_status NOT IN ('C','X'))"
+   " AND (coitem_scheddate BETWEEN <? value(\"startDate\") ?>"
+   "                           AND <? value(\"endDate\") ?>)"
+   "<? if exists(\"warehous_id\") ?>"
+   " AND (itemsite_warehous_id=<? value(\"warehous_id\") ?>)"
+   "<? endif ?>"
+   "<? if exists(\"custtype_id\") ?>"
+   " AND (cust_custtype_id=<? value(\"custtype_id\") ?>)"
+   "<? elseif exists(\"custtype_pattern\") ?>"
+   " AND (cust_custtype_id IN (SELECT custtype_id FROM custtype"
+   "  WHERE (custtype_code ~ <? value(\"custtype_pattern\") ?>)))"
+   "<? endif ?>"
+   "AND (cosmisc_shipped IS NOT NULL)) "
+   "GROUP BY cohead_id, salesrep_number, salesrep_name, cohead_number, cust_name,         "
+   "cohead_holdtype, cohead_orderdate, cohead_packdate,         "
+   "cosmisc_shipped, cosmisc_shipvia, cosmisc_shipdate,         "
+   "cosmisc_id, cobmisc_cohead_id, cohead_created, pack_head_id  ORDER BY           "
+   "cohead_number, cosmisc_shipped)");
 
     MetaSQLQuery mql(sql);
     q = mql.toQuery(params);
     if (q.first())
     {
-      double        totalSales  = 0.0;
-      double        totalCost   = 0.0;
-      double        totalMargin = 0.0;
-      int           coheadid    = -1;
-      bool          unshipped   = FALSE;
-      bool          overbilled  = FALSE;
-
-      do
-      {
-        if ( (coheadid != q.value("cohead_id").toInt()) || (!orderLine) )
-        {
-          coheadid = q.value("cohead_id").toInt();
-          unshipped = FALSE;
-          overbilled = FALSE;
-
-          orderLine = new XTreeWidgetItem( _so, orderLine,
-          q.value("cohead_id").toInt(), -1,
-          q.value("cohead_number"), q.value("cust_name"),
-          q.value("f_holdtype"), q.value("cohead_orderdate"),
-          q.value("scheddate"), q.value("cohead_packdate"),
-          formatMoney(q.value("sales").toDouble()),
-          formatMoney(q.value("cost").toDouble()),
-          formatMoney(q.value("margin").toDouble()),
-          q.value("cohead_created"), q.value("packed"));
-
-          totalSales  += q.value("sales").toDouble();
-          totalCost   += q.value("cost").toDouble();
-          totalMargin += q.value("margin").toDouble();
-        }
-
-        if (q.value("overbilled").toBool())
-        {
-          overbilled = TRUE;
-          orderLine->setTextColor("red");
-        }
-
-        if (q.value("shipstatus").toInt())
-        {
-          XTreeWidgetItem *shipLine = new XTreeWidgetItem(orderLine, -1,
-						 q.value("cosmisc_id").toInt());
-
-          if (q.value("shipstatus").toInt() == 1)
-          {
-            shipLine->setText(0, tr("Yes"));
-            shipLine->setTextColor("green");
-
-            if (!unshipped && !overbilled)
-              orderLine->setTextColor("green");
-          }
-          else if (q.value("shipstatus").toInt() == 2)
-          {
-            shipLine->setText(0, tr("No"));
-            shipLine->setTextColor("blue");
-            if(!overbilled)
-              orderLine->setTextColor("blue");
-            unshipped = TRUE;
-          }
-
-          shipLine->setText(1, q.value("shipvia"));
-	  shipLine->setText(2, q.value("cosmisc_number"));
-          shipLine->setText(3, q.value("shipdate"));
-        }
-      }
-      while (q.next());
-
-      if (_showPrices->isChecked())
-        new XTreeWidgetItem( _so, orderLine, -1,
-                           "", "", tr("Total Backlog"), "", "", "",
-                           formatMoney(totalSales),
-                           formatCost(totalCost),
-                           formatMoney(totalMargin) );
-
+   
       QString tots = "SELECT COUNT(cohead_id) AS totalorders "
 		     "FROM ( SELECT DISTINCT cohead_id "
 		     "       FROM cohead, coitem, itemsite, cust "
