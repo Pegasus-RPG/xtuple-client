d25d905bd4413cae08c7eb8fdf4a59d22a8d50e8
jrogelstad <jrogelstad>
2010-02-22 14:04:50 +0000

    issue #9780: Remove debug code.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@8185 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/returnAuthorizationItem.cpp
+++ guiclient/returnAuthorizationItem.cpp
@@ -704,7 +704,7 @@ void returnAuthorizationItem::sPopulateItemInfo()
     systemError(this, item.lastError().databaseText(), __FILE__, __LINE__);
     return;
   } 
-qDebug("test item " + _costmethod);
+
   if (_item->itemType() == "M" && _costmethod != "J")
     _createOrder->setEnabled(TRUE);
   else
@@ -739,10 +739,9 @@ void returnAuthorizationItem::sPopulateItemsiteInfo()
     {
       _leadTime = itemsite.value("itemsite_leadtime").toInt();
       _costmethod = itemsite.value("itemsite_costmethod").toString();
-qDebug("test itemsite" + _costmethod);
+
       if (_costmethod == "J")
       {
-        qDebug("is job");
         _createOrder->setChecked(TRUE);
         _createOrder->setEnabled(FALSE);
       }
