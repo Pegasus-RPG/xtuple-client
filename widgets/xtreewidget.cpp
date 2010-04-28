90b7abd6638918156b83848b9eeedbf692c7b495
gilmoskowitz <gilmoskowitz>
2010-04-28 15:46:39 +0000

    issue #8153 - some clients may not initialize _guiClientInterface
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@9100 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- widgets/xtreewidget.cpp
+++ widgets/xtreewidget.cpp
@@ -223,7 +223,8 @@ void XTreeWidget::populate(XSqlQuery pQuery, int pIndex, bool pUseAltId, Populat
     _workingParams.clear();
   _workingParams.append(args);
 
-  if(_guiClientInterface->globalQ()->result() == pQuery.result())
+  if (! _guiClientInterface ||
+      _guiClientInterface->globalQ()->result() == pQuery.result())
   {
     _linear = true;
     populateWorker();
