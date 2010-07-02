d4df3d53ddd5a2cab38d27992f6a422933292e91
cryan <cryan>
2010-07-02 18:44:55 +0000

    Fixed problem where some compilers where having odd behaviour with the loop used to calculate the precision multiplier. This should work correctly on all platforms. Issue #11699.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@9595 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- widgets/currCluster.cpp
+++ widgets/currCluster.cpp
@@ -712,11 +712,8 @@ void CurrDisplay::setLocalValue(double newValue)
   if (ABS(_valueLocal - newValue) > EPSILON(_localScale) ||
       newValue < EPSILON(_localScale))
   {
-    int prec = 1;
-    for (int cnt = 0; cnt < _decimals + _localScale; cnt++)
-      prec = prec * 10;
-    // Changed from qRound to floor because windows qRound was truncating on windows
-    _valueLocal = floor(newValue*prec + .5f)/prec;
+    double prec = pow(10, qMax(1, (_decimals + _localScale)));
+    _valueLocal = qRound(newValue*prec)/prec;
     _localKnown = true;
     emit valueLocalChanged(_valueLocal);
     sValueLocalChanged(_valueLocal);
