dc9e50b89a8d517e2addf12f4ac9e2dbeb043cd1
gilmoskowitz <gilmoskowitz>
2011-01-04 21:20:07 +0000

    issue #13123 and issue #12350 - need access to a qsqldatabase object in addition to qsqldatabase* so we can work with the default connection
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@11010 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- scriptapi/qsqldatabaseproto.cpp
+++ scriptapi/qsqldatabaseproto.cpp
@@ -8,17 +8,209 @@
  * to be bound by its terms.
  */
 
+#include "scriptapi_internal.h"
+
 #include "qsqldatabaseproto.h"
+
+/** \ingroup scriptapi
+    \brief   Script Exposure of QSqlDatabase class.
+
+    \todo implement QSqlDatabase::addDatabase(QSqlDriver*, const QString &connectionName)
+    \todo implement QSqlDatabase::registerSqlDriver(const QString &, QSqlDriverCreatorBase *)
+ */
+
+QScriptValue QSqlDatabasetoScriptValue(QScriptEngine *engine, QSqlDatabase const &item)
+{
+  QVariant v;
+  v.setValue(item);
+  return engine->newVariant(v);
+}
+
+void QSqlDatabasefromScriptValue(const QScriptValue &obj, QSqlDatabase &item)
+{
+  item = obj.toVariant().value<QSqlDatabase>();
+}
+
+QScriptValue QSqlDatabasePtrtoScriptValue(QScriptEngine *engine, QSqlDatabase* const &item)
+{
+  QVariant v;
+  v.setValue(item);
+  return engine->newVariant(v);
+}
+
+void QSqlDatabasePtrfromScriptValue(const QScriptValue &obj, QSqlDatabase* &item)
+{
+  item = obj.toVariant().value<QSqlDatabase*>();
+}
+
+static QScriptValue qsqldatabase_addDatabase(QScriptContext *context, QScriptEngine *engine)
+{
+  QSqlDatabase result;
+
+  if (context->argumentCount() == 1 && context->argument(0).isString())
+    result = QSqlDatabase::addDatabase(context->argument(0).toString());
+
+  else if (context->argumentCount() == 2 &&
+           context->argument(0).isString() &&
+           context->argument(1).isString())
+    result = QSqlDatabase::addDatabase(context->argument(0).toString(),
+                                       context->argument(1).toString());
+
+  /*
+  else if (context->argumentCount() == 1 &&
+           qscriptvalue_cast<QSqlDriver*>(context->argument(0)))
+    result = QSqlDatabase::addDatabase(qscriptvalue_cast<QSqlDriver*>(context->argument(0)));
+  else if (context->argumentCount() == 2 &&
+           qscriptvalue_cast<QSqlDriver*>(context->argument(0)) &&
+           context->argument(1).isString())
+    result = QSqlDatabase::addDatabase(qscriptvalue_cast<QSqlDriver*>(context->argument(0)),
+                                       context->argument(1).toString());
+   */
+
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::addDatabase()");
+
+  QVariant v;
+  v.setValue(result);
+  return engine->newVariant(v);
+}
+
+static QScriptValue qsqldatabase_cloneDatabase(QScriptContext *context, QScriptEngine *engine)
+{
+  QSqlDatabase result;
+
+  if (context->argumentCount() == 2 &&
+      // context->argument(0) is a QSqlDatabase // TODO: how do we know this?
+      context->argument(1).isString())
+    result = QSqlDatabase::cloneDatabase(qscriptvalue_cast<QSqlDatabase>(context->argument(0)),
+                                         context->argument(1).toString());
+
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::cloneDatabase()");
+
+  QVariant v;
+  v.setValue(result);
+  return engine->newVariant(v);
+}
+
+static QScriptValue qsqldatabase_connectionNames(QScriptContext * /*context*/, QScriptEngine *engine)
+{
+  QStringList result;
+
+  result = QSqlDatabase::connectionNames();
+
+  QVariant v;
+  v.setValue(result);
+  return engine->newVariant(v);
+}
+
+static QScriptValue qsqldatabase_contains(QScriptContext *context, QScriptEngine * /*engine*/)
+{
+  bool result = false;
+
+  if (context->argumentCount() == 1 && context->argument(0).isString())
+    result = QSqlDatabase::contains(context->argument(0).toString());
+  else
+    result = QSqlDatabase::contains();
+
+  return QScriptValue(result);
+}
+
+static QScriptValue qsqldatabase_database(QScriptContext *context, QScriptEngine *engine)
+{
+  QSqlDatabase result;
+
+  if (context->argumentCount() == 0)
+    result = QSqlDatabase::database();
+  else if (context->argumentCount() == 1 && context->argument(0).isString())
+    result = QSqlDatabase::database(context->argument(0).toString());
+  else if (context->argumentCount() == 2 &&
+           context->argument(0).isString() &&
+           context->argument(1).isBool())
+    result = QSqlDatabase::database(context->argument(0).toString(),
+                                    context->argument(1).toBool());
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::database()");
+
+  QVariant v;
+  v.setValue(result);
+  return engine->newVariant(v);
+}
+
+static QScriptValue qsqldatabase_drivers(QScriptContext * /*context*/, QScriptEngine *engine)
+{
+  QStringList result;
+
+  result = QSqlDatabase::drivers();
+
+  QVariant v;
+  v.setValue(result);
+  return engine->newVariant(v);
+}
+
+static QScriptValue qsqldatabase_isDriverAvailable(QScriptContext *context, QScriptEngine * /*engine*/)
+{
+  bool result = false;
+
+  if (context->argumentCount() == 1 && context->argument(0).isString())
+    result = QSqlDatabase::isDriverAvailable(context->argument(0).toString());
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::isDriverAvailable()");
+
+  return QScriptValue(result);
+}
+
+/*
+static QScriptValue qsqldatabase_registerSqlDriver(QScriptContext *context, QScriptEngine *engine)
+{
+  if (context->argumentCount() == 2)
+    QSqlDatabase::registerSqlDriver(context->argument(0).toString(), context->argument(1).toString());
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::registerSqlDriver()");
+
+  return QScriptValue();
+}
+*/
+
+static QScriptValue qsqldatabase_removeDatabase(QScriptContext *context, QScriptEngine * /*engine*/)
+{
+  if (context->argumentCount() == 1 && context->argument(0).isString())
+    QSqlDatabase::removeDatabase(context->argument(0).toString());
+  else
+    context->throwError(QScriptContext::UnknownError,
+                        "Could not find an appropriate QSqlDatabase::removeDatabase()");
+
+  return QScriptValue();
+}
 
 void setupQSqlDatabaseProto(QScriptEngine *engine)
 {
+  qScriptRegisterMetaType(engine, QSqlDatabasetoScriptValue,    QSqlDatabasefromScriptValue);
+  qScriptRegisterMetaType(engine, QSqlDatabasePtrtoScriptValue, QSqlDatabasePtrfromScriptValue);
+
   QScriptValue proto = engine->newQObject(new QSqlDatabaseProto(engine));
   engine->setDefaultPrototype(qMetaTypeId<QSqlDatabase*>(), proto);
   engine->setDefaultPrototype(qMetaTypeId<QSqlDatabase>(),  proto);
 
   QScriptValue constructor = engine->newFunction(constructQSqlDatabase,
                                                  proto);
-  engine->globalObject().setProperty("QSqlDatabase",  constructor);
+  engine->globalObject().setProperty("QSqlDatabase",  constructor, CTORPROPFLAGS);
+
+  constructor.setProperty("addDatabase",      engine->newFunction(qsqldatabase_addDatabase),      STATICPROPFLAGS);
+  constructor.setProperty("cloneDatabase",    engine->newFunction(qsqldatabase_cloneDatabase),    STATICPROPFLAGS);
+  constructor.setProperty("connectionNames",  engine->newFunction(qsqldatabase_connectionNames),  STATICPROPFLAGS);
+  constructor.setProperty("contains",         engine->newFunction(qsqldatabase_contains),         STATICPROPFLAGS);
+  constructor.setProperty("database",         engine->newFunction(qsqldatabase_database),         STATICPROPFLAGS);
+  constructor.setProperty("drivers",          engine->newFunction(qsqldatabase_drivers),          STATICPROPFLAGS);
+  constructor.setProperty("isDriverAvailable",engine->newFunction(qsqldatabase_isDriverAvailable),STATICPROPFLAGS);
+//constructor.setProperty("registerSqlDriver",engine->newFunction(qsqldatabase_registerSqlDriver),STATICPROPFLAGS);
+  constructor.setProperty("removeDatabase",   engine->newFunction(qsqldatabase_removeDatabase),   STATICPROPFLAGS);
+
 }
 
 QScriptValue constructQSqlDatabase(QScriptContext *context,
@@ -42,6 +234,8 @@ void QSqlDatabaseProto::close()
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->close();
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().close();
 }
 
 bool QSqlDatabaseProto::commit()
@@ -49,6 +243,8 @@ bool QSqlDatabaseProto::commit()
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->commit();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().commit();
   return false;
 }
 
@@ -57,6 +253,8 @@ QString QSqlDatabaseProto::connectOptions()   const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->connectOptions();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().connectOptions();
   return QString();
 }
 
@@ -65,6 +263,8 @@ QString QSqlDatabaseProto::connectionName()   const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->connectionName();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().connectionName();
   return QString();
 }
 
@@ -73,6 +273,8 @@ QString QSqlDatabaseProto::databaseName()     const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->databaseName();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().databaseName();
   return QString();
 }
 
@@ -81,6 +283,8 @@ QSqlDriver *QSqlDatabaseProto::driver()           const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->driver();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().driver();
   return 0;
 }
 
@@ -89,6 +293,8 @@ QString QSqlDatabaseProto::driverName()       const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->driverName();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().driverName();
   return QString();
 }
 
@@ -97,6 +303,8 @@ QSqlQuery QSqlDatabaseProto::exec(const QString &query) const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->exec(query);
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().exec(query);
   return QSqlQuery();
 }
 
@@ -105,6 +313,8 @@ QString QSqlDatabaseProto::hostName()         const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->hostName();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().hostName();
   return QString();
 }
 
@@ -113,6 +323,8 @@ bool QSqlDatabaseProto::isOpen()           const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->isOpen();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().isOpen();
   return false;
 }
 
@@ -121,6 +333,8 @@ bool QSqlDatabaseProto::isOpenError()      const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->isOpenError();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().isOpenError();
   return false;
 }
 
@@ -129,6 +343,8 @@ bool QSqlDatabaseProto::isValid()          const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->isValid();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().isValid();
   return false;
 }
 
@@ -137,6 +353,8 @@ QSqlError QSqlDatabaseProto::lastError()        const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->lastError();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().lastError();
   return QSqlError();
 }
 
@@ -145,6 +363,8 @@ bool QSqlDatabaseProto::open()
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->open();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().open();
   return false;
 }
 
@@ -153,6 +373,8 @@ bool QSqlDatabaseProto::open(const QString &user, const QString &password)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->open(user, password);
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().open(user, password);
   return false;
 }
 
@@ -161,6 +383,8 @@ QString QSqlDatabaseProto::password()         const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->password();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().password();
   return QString();
 }
 
@@ -169,6 +393,8 @@ int QSqlDatabaseProto::port()             const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->port();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().port();
   return 0;
 }
 
@@ -177,6 +403,8 @@ QSqlIndex QSqlDatabaseProto::primaryIndex(const QString &tablename) const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->primaryIndex(tablename);
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().primaryIndex(tablename);
   return QSqlIndex();
 }
 
@@ -185,6 +413,8 @@ QSqlRecord QSqlDatabaseProto::record(const QString &tablename)       const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->record(tablename);
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().record(tablename);
   return QSqlRecord();
 }
 
@@ -193,6 +423,8 @@ bool QSqlDatabaseProto::rollback()
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->rollback();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().rollback();
   return false;
 }
 
@@ -201,6 +433,8 @@ void QSqlDatabaseProto::setConnectOptions(const QString &options)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setConnectOptions(options);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setConnectOptions(options);
 }
 
 void QSqlDatabaseProto::setDatabaseName(const QString &name)
@@ -208,6 +442,8 @@ void QSqlDatabaseProto::setDatabaseName(const QString &name)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setDatabaseName(name);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setDatabaseName(name);
 }
 
 void QSqlDatabaseProto::setHostName(const QString &host)
@@ -215,6 +451,8 @@ void QSqlDatabaseProto::setHostName(const QString &host)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setHostName(host);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setHostName(host);
 }
 
 void QSqlDatabaseProto::setPassword(const QString &password)
@@ -222,6 +460,8 @@ void QSqlDatabaseProto::setPassword(const QString &password)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setPassword(password);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setPassword(password);
 }
 
 void QSqlDatabaseProto::setPort(int port)
@@ -229,6 +469,8 @@ void QSqlDatabaseProto::setPort(int port)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setPort(port);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setPort(port);
 }
 
 void QSqlDatabaseProto::setUserName(const QString &name)
@@ -236,6 +478,8 @@ void QSqlDatabaseProto::setUserName(const QString &name)
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     item->setUserName(name);
+  else if (! thisObject().isNull())
+    thisObject().toVariant().value<QSqlDatabase>().setUserName(name);
 }
 
 QStringList QSqlDatabaseProto::tables(int type) const
@@ -243,6 +487,8 @@ QStringList QSqlDatabaseProto::tables(int type) const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->tables((QSql::TableType)type);
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().tables((QSql::TableType)type);
   return QStringList();
 }
 
@@ -251,6 +497,8 @@ bool QSqlDatabaseProto::transaction()
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->transaction();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().transaction();
   return false;
 }
 
@@ -259,6 +507,8 @@ QString QSqlDatabaseProto::userName() const
   QSqlDatabase *item = qscriptvalue_cast<QSqlDatabase*>(thisObject());
   if (item)
     return item->userName();
+  else if (! thisObject().isNull())
+    return thisObject().toVariant().value<QSqlDatabase>().userName();
   return QString();
 }
 
