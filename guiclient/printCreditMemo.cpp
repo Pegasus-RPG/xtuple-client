69a6cc7e0579221de0b963f536570de625aa5920
sunsetsolutions <sunsetsolutions>
2011-03-04 18:28:49 +0000

    Issue #14047:move update and post outside of print loop
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@11486 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/printCreditMemo.cpp
+++ guiclient/printCreditMemo.cpp
@@ -167,59 +167,59 @@ void printCreditMemo::sPrint()
 	    orReport::endMultiPrint(&_printer);
           return;
         }
+      }
+    }
 
-        q.prepare( "UPDATE cmhead "
-                   "SET cmhead_printed=TRUE "
-                   "WHERE (cmhead_id=:cmhead_id);" );
-        q.bindValue(":cmhead_id", _cmheadid);
-        q.exec();
-	if (q.lastError().type() != QSqlError::NoError)
-	{
-	  systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
-	  return;
-	}
+    q.prepare( "UPDATE cmhead "
+               "SET cmhead_printed=TRUE "
+               "WHERE (cmhead_id=:cmhead_id);" );
+    q.bindValue(":cmhead_id", _cmheadid);
+    q.exec();
+    if (q.lastError().type() != QSqlError::NoError)
+    {
+      systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+      return;
+    }
 
-        if (_alert)
-          omfgThis->sCreditMemosUpdated();
-        
-        if (_post->isChecked())
+    if (_alert)
+      omfgThis->sCreditMemosUpdated();
+
+    if (_post->isChecked())
+    {
+      q.exec("BEGIN;");
+      //TO DO:  Replace this method with commit that doesn't require transaction
+      //block that can lead to locking issues
+      XSqlQuery rollback;
+      rollback.prepare("ROLLBACK;");
+
+      q.prepare("SELECT postCreditMemo(:cmhead_id, 0) AS result;");
+      q.bindValue(":cmhead_id", _cmheadid);
+      q.exec();
+      q.first();
+      int result = q.value("result").toInt();
+      if (result < 0)
+      {
+        rollback.exec();
+        systemError( this, storedProcErrorLookup("postCreditMemo", result),
+              __FILE__, __LINE__);
+        return;
+      }
+      else if (q.lastError().type() != QSqlError::NoError)
+      {
+        systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+        rollback.exec();
+        return;
+      }
+      else
+      {
+        if (distributeInventory::SeriesAdjust(result, this) == XDialog::Rejected)
         {
-          q.exec("BEGIN;");
-          //TO DO:  Replace this method with commit that doesn't require transaction
-          //block that can lead to locking issues
-          XSqlQuery rollback;
-          rollback.prepare("ROLLBACK;");
-          
-          q.prepare("SELECT postCreditMemo(:cmhead_id, 0) AS result;");
-          q.bindValue(":cmhead_id", _cmheadid);
-          q.exec();
-          q.first();
-          int result = q.value("result").toInt();
-          if (result < 0)
-          {
-            rollback.exec();
-            systemError( this, storedProcErrorLookup("postCreditMemo", result),
-                  __FILE__, __LINE__);
-            return;
-          }
-          else if (q.lastError().type() != QSqlError::NoError)
-	  {
-	    systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
-            rollback.exec();
-	    return;
-	  }
-          else
-          {
-            if (distributeInventory::SeriesAdjust(result, this) == XDialog::Rejected)
-            {
-              rollback.exec();
-              QMessageBox::information( this, tr("Post Credit Memo"), tr("Transaction Canceled") );
-              return;
-            }
-
-            q.exec("COMMIT;");
-          }         
+          rollback.exec();
+          QMessageBox::information( this, tr("Post Credit Memo"), tr("Transaction Canceled") );
+          return;
         }
+
+        q.exec("COMMIT;");
       }
     }
 
