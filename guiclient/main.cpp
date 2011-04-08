8001d96c597c30586217842643e1622cd0d44b4a
cryan <cryan>
2011-04-08 13:01:14 +0000

    Added client version to the information being passed back to the violation record.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@11800 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/main.cpp
+++ guiclient/main.cpp
@@ -390,6 +390,7 @@ int main(int argc, char *argv[])
       url.addQueryItem("dbname", QUrl::toPercentEncoding(dbname));
       url.addQueryItem("db", QUrl::toPercentEncoding(db));
       url.addQueryItem("cnt", QString::number(cnt));
+      url.addQueryItem("ver", _Version);
 
       http->setHost("www.xtuple.org");
       http->get(url.toString());
