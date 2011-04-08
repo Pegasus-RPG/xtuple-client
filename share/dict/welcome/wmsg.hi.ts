f11acd53f3e6966a286b9ed8ba6f8acd0665641c
cryan <cryan>
2011-04-08 12:45:05 +0000

    Added hindi translation.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@11799 ea1a15ef-c238-0410-93cb-fa28e9f07733

---
+++ share/dict/welcome/wmsg.hi.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hi" sourcelanguage="en">
+<context>
+    <name>welcomeStub</name>
+    <message>
+        <source>Welcome</source>
+        <translation>पधारीए</translation>
+    </message>
+    <message>
+        <source>Do not show again.</source>
+        <translation>दुबारा मत दिखाना</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;It appears you do not have an xTuple translation file installed on your system. Please use the following link to find a translation file for your language and learn more about how to install translation files in xTuple ERP:&lt;p&gt;%1&lt;p&gt;</source>
+        <translation>&lt;p&gt;लगता है की आपकी सीस्टम में भाषान्तर कि फाइल मोजुद नही है। यह लिंक का इस्तमाल करके फाइल को डाउनलोड करें और इससे कैसे इन्स्टोल करें वो जानीए।&lt;p&gt;%1&lt;p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>QDialogButtonBox</name>
+    <message>
+        <source>OK</source>
+        <translation>बराबर / ओके</translation>
+    </message>
+    <message>
+        <source>&amp;OK</source>
+        <translation>बराबर / ओके</translation>
+    </message>
+</context>
+</TS>
