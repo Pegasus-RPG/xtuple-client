64018584e2f244e0d67b6cc592878c72d206060c
sunsetsolutions <sunsetsolutions>
2011-04-22 19:54:11 +0000

    Issue #14581:process the _period list in period_start order
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@11867 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/syncCompanies.cpp
+++ guiclient/syncCompanies.cpp
@@ -448,7 +448,23 @@ void syncCompanies::sSync()
       for (int i = 0; i < _period->topLevelItemCount(); i++)
       {
         if (_period->topLevelItem(i)->isSelected())
-          period.append(_period->topLevelItem(i));
+        {
+          bool inserted = false;
+          QDate _periodStart = _period->topLevelItem(i)->rawValue("period_start").toDate();
+          for (int j = 0; j < period.size(); j++)
+          {
+            XTreeWidgetItem *p = (XTreeWidgetItem*)(period[j]);
+            QDate periodStart = p->rawValue("period_start").toDate();
+            if (_periodStart < periodStart)
+            {
+              period.insert(j, _period->topLevelItem(i));
+              inserted = true;
+              break;
+            }
+          }
+          if (!inserted)
+            period.append(_period->topLevelItem(i));
+        }
       }
 
       for (int j = 0; j < period.size(); j++)
