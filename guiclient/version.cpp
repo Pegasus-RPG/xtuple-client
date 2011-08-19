04e022df39bdbd8051aa267cc11997e811402c59
cryan <cryan>
2011-08-19 12:40:33 +0000

    Updated translations and versions for release.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@12321 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -13,8 +13,8 @@
 QString _Name = "xTuple ERP: %1 Edition";
 
 #ifndef __USEALTVERSION__
-QString _Version   = "3.7.0";
-QString _dbVersion = "3.7.0";
+QString _Version   = "3.8.0Alpha";
+QString _dbVersion = "3.8.0Alpha";
 
 #else
 #include "../altVersion.cpp"
