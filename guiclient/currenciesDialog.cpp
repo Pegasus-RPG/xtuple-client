7e1c1b61d75e5f5904ae234ae242855b724329e2
clinuz <clinuz>
2011-10-14 20:47:33 +0000

    issue #15726: Need to include required files with commit.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/trunk@12633 ea1a15ef-c238-0410-93cb-fa28e9f07733

---
+++ guiclient/currenciesDialog.cpp
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+#include "currenciesDialog.h"
+#include "currencies.h"
+
+currenciesDialog::currenciesDialog(QWidget* parent, const char* name, bool modal, Qt::WFlags fl)
+    : QDialog(parent, modal ? (fl | Qt::Dialog) : fl)
+{
+  this->setFixedSize(400, 400);
+  _layout = new QBoxLayout(QBoxLayout::TopToBottom, this);
+  _currencies = new currencies(this, "", Qt::Widget);
+  _msg = new QLabel(tr("Please select a base currency for this database."), this);
+  _layout->addWidget(_msg);
+  _layout->addWidget(_currencies);
+  connect(_currencies->_close, SIGNAL(clicked()), this, SLOT(close()));
+}
+
+currenciesDialog::~currenciesDialog()
+{
+  // no need to delete child widgets, Qt does it all for us
+}
