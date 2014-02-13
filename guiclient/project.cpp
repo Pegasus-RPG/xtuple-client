f92fb25560d7d7667907d059c653b8adbe97f914
garyhgohoos <gary@xtuple.com>
2014-02-13 10:59:09 -0500

    Issue #22211:allow null in _prjtype combo

--- guiclient/project.cpp
+++ guiclient/project.cpp
@@ -420,9 +420,7 @@ void project::populate()
   }
 
   XSqlQuery projectType;
-  projectType.prepare( "SELECT -1 AS prjtype_id, 'Undefined' AS prjtype_desc "
-                       "UNION "
-                       "SELECT prjtype_id, prjtype_descr FROM prjtype WHERE prjtype_active "
+  projectType.prepare( "SELECT prjtype_id, prjtype_descr FROM prjtype WHERE prjtype_active "
                        "UNION "
                        "SELECT prjtype_id, prjtype_descr FROM prjtype "
                        "JOIN prj ON (prj_prjtype_id=prjtype_id) "
