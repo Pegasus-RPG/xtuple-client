1d10b4c75a122385cd3fe51a6115ed66ff216f3c
Greg Pazo <gpazo@xtuple.com>
2014-12-22 14:29:33 -0500

    bump to 4.8.0

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -11,8 +11,8 @@
 #include "version.h"
 
 QString _Name      = "xTuple ERP: %1 Edition";
-QString _Version   = "4.8.0Beta";
-QString _dbVersion = "4.8.0Beta";
+QString _Version   = "4.8.0";
+QString _dbVersion = "4.8.0";
 QString _Copyright = "Copyright (c) 1999-2014, OpenMFG, LLC.";
 
 #ifdef __USEALTVERSION__
