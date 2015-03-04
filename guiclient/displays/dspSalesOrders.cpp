953a790f6d5108e4d3bf6a7b97b10e93ef9aaed3
Greg Pazo <gpazo@xtuple.com>
2015-03-04 11:49:39 -0500

    another qt5 fix

--- guiclient/displays/dspSalesOrders.cpp
+++ guiclient/displays/dspSalesOrders.cpp
@@ -123,7 +123,7 @@ void dspSalesOrders::sCopyOrder()
   ParameterList params;
   params.append("sohead_id", list()->id());
       
-  copySalesOrder newdlg(this, "", TRUE);
+  copySalesOrder newdlg(this, "", true);
   newdlg.set(params);
   newdlg.exec();
 }
