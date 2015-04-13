ed2d9f8df1d1b0744a106968ad9a0ce2374dadb8
Gil Moskowitz <gilmoskowitz@acm.org>
2015-04-13 12:48:06 -0400

    simplify applock callers with revised api

--- guiclient/salesOrder.cpp
+++ guiclient/salesOrder.cpp
@@ -1257,10 +1257,9 @@ bool salesOrder::save(bool partial)
 
   // TODO: should this be done before saveSales.exec()?
   if ((cNew == _mode) && (!_saved)
-      && ! _lock.acquire(ISORDER(_mode) ? "cohead" : "quhead", _soheadid))
+      && ! _lock.acquire(ISORDER(_mode) ? "cohead" : "quhead", _soheadid,
+                         AppLock::Interactive))
   {
-    ErrorReporter::error(QtCriticalMsg, this, tr("Locking Error"),
-                         _lock.lastError(), __FILE__, __LINE__);
     return false;
   }
 
@@ -2388,21 +2387,10 @@ void salesOrder::populate()
   {
     XSqlQuery so;
     if (_mode == cEdit
-        && !_lock.acquire(ISORDER(_mode) ? "cohead" : "quhead", _soheadid))
+        && !_lock.acquire(ISORDER(_mode) ? "cohead" : "quhead", _soheadid,
+                          AppLock::Interactive))
     {
-      if (_lock.isLockedOut())
-      {
-        QMessageBox::critical(this, tr("Record Currently Being Edited"),
-                              tr("<p>The record you are trying to edit is "
-                                 "currently being edited by another user. "
-                                 "Continue in View Mode.") );
-        setViewMode();
-      }
-      else if (ErrorReporter::error(QtCriticalMsg, this, tr("Locking Error"),
-                                    _lock.lastError(), __FILE__, __LINE__))
-      {
-        setViewMode();
-      }
+      setViewMode();
     }
     so.prepare( "SELECT cohead.*,"
                 "       COALESCE(cohead_shipto_id,-1) AS cohead_shipto_id,"
