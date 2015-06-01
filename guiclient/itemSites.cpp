6392f13dc6abf40128ee0ca5ce013fade6f67861
Dave Anderson (Pentuple) <dave@pentuple.co.nz>
2015-06-02 11:05:52 +1200

    Issue #25914: Add Loc Ctrl parameter to ItemSites screen

--- guiclient/itemSites.cpp
+++ guiclient/itemSites.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2015 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -41,6 +41,7 @@ itemSites::itemSites(QWidget* parent, const char*, Qt::WindowFlags fl)
   parameterWidget()->append(tr("Item"), "item_id", ParameterWidget::Item);
   parameterWidget()->appendComboBox(tr("Planner Code"), "plancode_id", XComboBox::PlannerCodes);
   parameterWidget()->append(tr("Planner Code Pattern"), "plancode_pattern", ParameterWidget::Text);
+  parameterWidget()->append(tr("Location Controlled"), "loc_cntrl", ParameterWidget::CheckBox);
   parameterWidget()->append(tr("Show Inactive"), "showInactive", ParameterWidget::Exists);
   if (_metrics->boolean("MultiWhs"))
     parameterWidget()->append(tr("Site"), "warehous_id", ParameterWidget::Site);
