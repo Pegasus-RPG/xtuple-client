d63d2f34d97e5d665bcd41f45a6979147c95b4d6
Ben Thompson <ben@xtuple.com>
2015-06-30 15:37:41 -0400

    Expose all of QWebFrame's Public Functions to Qt Scripting.

---
+++ scriptapi/qwebframeproto.cpp
@@ -0,0 +1,406 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which(including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+#include "qwebframeproto.h"
+
+#include <QIcon>
+#include <QList>
+#include <QMultiMap>
+#include <QPoint>
+#include <QRect>
+#include <QSize>
+#include <QString>
+#include <QUrl>
+#include <QWebElement>
+#include <QWebElementCollection>
+#include <QWebHitTestResult>
+#include <QWebFrame>
+#include <QWebPage>
+#include <QWebSecurityOrigin>
+
+QScriptValue QWebFrametoScriptValue(QScriptEngine *engine, QWebFrame* const &item)
+{
+  return engine->newQObject(item);
+}
+
+void QWebFramefromScriptValue(const QScriptValue &obj, QWebFrame* &item)
+{
+  item = qobject_cast<QWebFrame*>(obj.toQObject());
+}
+
+void setupQWebFrameProto(QScriptEngine *engine)
+{
+  qScriptRegisterMetaType(engine, QWebFrametoScriptValue, QWebFramefromScriptValue);
+
+  QScriptValue proto = engine->newQObject(new QWebFrameProto(engine));
+  engine->setDefaultPrototype(qMetaTypeId<QWebFrame*>(), proto);
+
+  QScriptValue constructor = engine->newFunction(constructQWebFrame,
+                                                 proto);
+  engine->globalObject().setProperty("QWebFrame",  constructor);
+}
+
+QScriptValue constructQWebFrame(QScriptContext * context,
+                                    QScriptEngine  *engine)
+{
+  QWebFrame *obj = 0;
+
+  return engine->toScriptValue(obj);
+}
+
+QWebFrameProto::QWebFrameProto(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void QWebFrameProto::addToJavaScriptWindowObject(const QString & name, QObject * object, QWebFrame::ValueOwnership own)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->addToJavaScriptWindowObject(name, object, own);
+}
+
+QUrl QWebFrameProto::baseUrl() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->baseUrl();
+  return QUrl();
+}
+
+QList<QWebFrame *> QWebFrameProto::childFrames() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->childFrames();
+  return QList<QWebFrame *>();
+}
+
+QSize QWebFrameProto::contentsSize() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->contentsSize();
+  return QSize();
+}
+
+QWebElement QWebFrameProto::documentElement() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->documentElement();
+  return QWebElement();
+}
+
+QWebElementCollection QWebFrameProto::findAllElements(const QString & selectorQuery) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->findAllElements(selectorQuery);
+  return QWebElementCollection();
+}
+
+QWebElement QWebFrameProto::findFirstElement(const QString & selectorQuery) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->findFirstElement(selectorQuery);
+  return QWebElement();
+}
+
+QString QWebFrameProto::frameName() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->frameName();
+  return QString();
+}
+
+QRect QWebFrameProto::geometry() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->geometry();
+  return QRect();
+}
+
+bool QWebFrameProto::hasFocus() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->hasFocus();
+  return false;
+}
+
+/* TODO - else no `item` return what? */
+QWebHitTestResult QWebFrameProto::hitTestContent(const QPoint & pos) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->hitTestContent(pos);
+  return QWebHitTestResult();
+}
+
+QIcon QWebFrameProto::icon() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->icon();
+  return QIcon();
+}
+
+void QWebFrameProto::load(const QUrl & url)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->load(url);
+}
+
+void QWebFrameProto::load(const QNetworkRequest & req, QNetworkAccessManager::Operation operation, const QByteArray & body)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->load(req, operation, body);
+}
+
+QMultiMap<QString, QString> QWebFrameProto::metaData() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->metaData();
+  return QMultiMap<QString, QString>();
+}
+
+QWebPage* QWebFrameProto::page() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->page();
+  return 0;
+}
+
+QWebFrame* QWebFrameProto::parentFrame() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->parentFrame();
+  return 0;
+}
+
+QPoint QWebFrameProto::pos() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->pos();
+  return QPoint();
+}
+
+void QWebFrameProto::render(QPainter * painter, const QRegion & clip)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->render(painter, clip);
+}
+
+void QWebFrameProto::render(QPainter * painter, QWebFrame::RenderLayers layer, const QRegion & clip)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->render(painter, layer, clip);
+}
+
+QUrl QWebFrameProto::requestedUrl() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->requestedUrl();
+  return QUrl();
+}
+
+void QWebFrameProto::scroll(int dx, int dy)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scroll(dx, dy);
+}
+
+QRect QWebFrameProto::scrollBarGeometry(Qt::Orientation orientation) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollBarGeometry(orientation);
+  return QRect();
+}
+
+int QWebFrameProto::scrollBarMaximum(Qt::Orientation orientation) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollBarMaximum(orientation);
+  return 0;
+}
+
+int QWebFrameProto::scrollBarMinimum(Qt::Orientation orientation) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollBarMinimum(orientation);
+  return 0;
+}
+
+Qt::ScrollBarPolicy QWebFrameProto::scrollBarPolicy(Qt::Orientation orientation) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollBarPolicy(orientation);
+  return Qt::ScrollBarPolicy();
+}
+
+int QWebFrameProto::scrollBarValue(Qt::Orientation orientation) const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollBarValue(orientation);
+  return 0;
+}
+
+QPoint QWebFrameProto::scrollPosition() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollPosition();
+  return QPoint();
+}
+
+void QWebFrameProto::scrollToAnchor(const QString & anchor)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->scrollToAnchor(anchor);
+}
+
+/* TODO - else no `item` return what? */
+QWebSecurityOrigin QWebFrameProto::securityOrigin() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->securityOrigin();
+}
+
+void QWebFrameProto::setContent(const QByteArray & data, const QString & mimeType, const QUrl & baseUrl)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setContent(data, mimeType, baseUrl);
+}
+
+void QWebFrameProto::setFocus()
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setFocus();
+}
+
+void QWebFrameProto::setHtml(const QString & html, const QUrl & baseUrl)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setHtml(html, baseUrl);
+}
+
+void QWebFrameProto::setScrollBarPolicy(Qt::Orientation orientation, Qt::ScrollBarPolicy policy)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setScrollBarPolicy(orientation, policy);
+}
+
+void QWebFrameProto::setScrollBarValue(Qt::Orientation orientation, int value)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setScrollBarValue(orientation, value);
+}
+
+void QWebFrameProto::setScrollPosition(const QPoint & pos)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setScrollPosition(pos);
+}
+
+void QWebFrameProto::setTextSizeMultiplier(qreal factor)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setTextSizeMultiplier(factor);
+}
+
+void QWebFrameProto::setUrl(const QUrl & url)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setUrl(url);
+}
+
+void QWebFrameProto::setZoomFactor(qreal factor)
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->setZoomFactor(factor);
+}
+
+qreal QWebFrameProto::textSizeMultiplier() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->textSizeMultiplier();
+  return qreal();
+}
+
+QString QWebFrameProto::title() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->title();
+  return QString();
+}
+
+QString QWebFrameProto::toHtml() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->toHtml();
+  return QString();
+}
+
+QString QWebFrameProto::toPlainText() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->toPlainText();
+  return QString();
+}
+
+QUrl QWebFrameProto::url() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->url();
+  return QUrl();
+}
+
+qreal QWebFrameProto::zoomFactor() const
+{
+  QWebFrame *item = qscriptvalue_cast<QWebFrame*>(thisObject());
+  if (item)
+    return item->zoomFactor();
+  return qreal();
+}
