6331e5259bb58cf9e75630922639fcd40862a242
garyhgohoos <gary@xtuple.com>
2015-09-16 10:03:29 -0400

    Issue #26528:synchronize inventory and price uom

--- guiclient/item.cpp
+++ guiclient/item.cpp
@@ -845,9 +845,9 @@ void item::sSave()
   itemSave.exec();
   if (itemSave.lastError().type() != QSqlError::NoError)
   {
+    systemError(this, itemSave.lastError().databaseText(), __FILE__, __LINE__);
     itemSave.exec("ROLLBACK;");
     _inTransaction = false;
-    systemError(this, itemSave.lastError().databaseText(), __FILE__, __LINE__);
     return;
   }
 
@@ -1148,8 +1148,7 @@ void item::sPopulateUOMs()
   {
     saveCore();
     sPopulatePriceUOMs();
-    if (_priceUOM->id()==-1)
-      _priceUOM->setId(_inventoryUOM->id());
+    _priceUOM->setId(_inventoryUOM->id());
   }
 }
 
@@ -1913,7 +1912,7 @@ void item::sPopulatePriceUOMs()
   ParameterList params;
   params.append("uomtype", "Selling");
   params.append("item_id", _itemid);
-  params.append("uom_id", _priceUOM->id());
+  params.append("uom_id", _inventoryUOM->id());
 
   XSqlQuery puom = muom.toQuery(params);
   _priceUOM->populate(puom);
