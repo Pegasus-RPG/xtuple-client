cc5d18b8235bfbb4cf30ea90b56c3d3809d9c02c
Jeff Rosengarden <jrosengarden@erpdevelopers.net>
2015-12-02 11:23:11 -0600

    errorReporterRefactor  12/02/15 from Win10 - changes per Gil

--- guiclient/group.cpp
+++ guiclient/group.cpp
@@ -113,10 +113,11 @@ enum SetResponse group::set(const ParameterList &pParams)
                  "VALUES( :grp_id, :grp_id, '' );" );
       groupet.bindValue(":grp_id", _grpid);
       groupet.exec();
-      if (ErrorReporter::error(QtCriticalMsg, this, tr("Error Retrieving Group Information"),
-                                    groupet, __FILE__, __LINE__))
+      if (groupet.lastError().type() != QSqlError::NoError)
       {
         groupet.exec("ROLLBACK;");
+        ErrorReporter::error(QtCriticalMsg, this, tr("Error Retrieving Group Information"),
+                             groupet, __FILE__, __LINE__);
         _trapClose = false;
         return UndefinedError;
       }
