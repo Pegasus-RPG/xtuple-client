383796a02c180c2987e3e53f2bae488e7af2d5aa
garyhgohoos <gary@xtuple.com>
2016-02-22 13:10:44 -0500

    Issue #27055:disable QWheelEvent

--- widgets/xcombobox.cpp
+++ widgets/xcombobox.cpp
@@ -1598,6 +1598,10 @@ void XComboBox::mousePressEvent(QMouseEvent *event)
   QComboBox::mousePressEvent(event);
 }
 
+void XComboBox::wheelEvent(QWheelEvent *event)
+{
+}
+
 void XComboBox::showPopup()
 {
   QComboBox::showPopup();
