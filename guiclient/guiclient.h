0a9a6158da2d7335a6c596561090422eb5805d45
Gil Moskowitz <gilmoskowitz@acm.org>
2016-05-25 09:23:37 -0400

    issue #27953 - remove unused class member

--- guiclient/guiclient.h
+++ guiclient/guiclient.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -13,7 +13,6 @@
 
 #include <QAction>
 #include <QDate>
-#include <QList>
 #include <QMainWindow>
 #include <QTimer>
 
@@ -409,7 +408,6 @@ class GUIClient : public QMainWindow
     bool         _showTopLevel;
     QWidgetList  _windowList;
     QMenuBar	*_menuBar;
-    QWidget     *_activeWindow;
 
     InputManager   *_inputManager;
 
