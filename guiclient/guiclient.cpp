50f4e43e78d3014c9d311065cbec8d37c82b49bd
Chris Cotnoir <ccotnoir@xtuple.com>
2016-09-13 12:06:26 -0400

    Used more clear code

--- guiclient/guiclient.cpp
+++ guiclient/guiclient.cpp
@@ -896,12 +896,15 @@ void GUIClient::showEvent(QShowEvent *event)
       XSqlQuery sq;
       sq.prepare("SELECT script_source "
                  "  FROM script "
+                 "JOIN (SELECT relfilenode, n.nspname || '.' || c.relname AS schema_table "
+                 "  FROM pg_class AS c "
+                 "  JOIN pg_namespace AS n ON c.relnamespace=n.oid) AS schema_table "
+                 "ON script.tableoid=relfilenode "
                  "JOIN (SELECT regexp_split_to_table AS pkgname, row_number() over () AS seq "
                  "  FROM regexp_split_to_table(buildsearchpath(), ',') "
                  "  UNION SELECT 'xtcore', 0) AS path "
-                 "ON pkgname || '.pkgscript' = tableoid::regclass::text "
-                 "OR (pkgname = 'public' AND tableoid::regclass::text = 'script') "
-                 "OR (tableoid::regclass::text = 'pkgscript' AND seq=1) "
+                 "ON pkgname || '.pkgscript' = schema_table "
+                 "OR (pkgname = 'public' AND schema_table = 'public.script') "
                  " WHERE script_enabled AND script_name = 'initMenu' "
                  "ORDER BY script_order, seq;");
       sq.exec();
