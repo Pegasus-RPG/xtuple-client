916e8b5738d4c1a92113ec6718437205cc1a948f
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-01-24 07:57:10 +1300

    Add variable initialisation to constructor

--- guiclient/itemSite.cpp
+++ guiclient/itemSite.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -117,8 +117,10 @@ itemSite::itemSite(QWidget* parent, const char* name, bool modal, Qt::WindowFlag
   _controlMethod->append(2, tr("Lot #"),    "L");
   _controlMethod->append(3, tr("Serial #"), "S");
 
-  //Default to Regular control  
+  //Default to Regular control
   _controlMethod->setCode("R");
+
+  _wasControlMethod = "";
 
   //If not lot serial control, remove options
   if (!_metrics->boolean("LotSerialControl"))
