b55100d830605652d2a26d747dd7cddd85b540fa
Gil Moskowitz <gilmoskowitz@acm.org>
2017-05-25 17:09:38 -0400

    bump in anticipation of next release

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -11,9 +11,9 @@
 #include "version.h"
 
 QString _Name        = "xTuple ERP: %1 Edition";
-QString _Version     = "4.11.0Beta";
-QString _dbVersion   = "4.11.0Beta";
-QString _Copyright   = "Copyright (c) 1999-2016, OpenMFG, LLC.";
+QString _Version     = "4.11.0RC";
+QString _dbVersion   = "4.11.0RC";
+QString _Copyright   = "Copyright (c) 1999-2017, OpenMFG, LLC.";
 QString _ConnAppName = "xTuple ERP (qt-client)";
 
 #ifdef __USEALTVERSION__
@@ -21,10 +21,3 @@ QString _ConnAppName = "xTuple ERP (qt-client)";
 #else
 QString _Build     = "%1 %2";
 #endif
-
-/*: Please translate this Version string to the base version of the application
-    you are translating. This is a hack to embed the application version number
-    into the translation file so the Update Manager can find
-    the best translation file for a given version of the application.
- */
-static QString _translationFileVersionPlaceholder = QT_TRANSLATE_NOOP("xTuple", "Version");
