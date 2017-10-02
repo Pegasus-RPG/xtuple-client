a14b431b157a0779d8a4c90d4e82e39340db38f6
jcarlin <jcarlin@xtuple.com>
2017-10-02 17:00:37 -0500

    add error text

--- common/storedProcErrorLookup.cpp
+++ common/storedProcErrorLookup.cpp
@@ -909,6 +909,9 @@ const struct {
   { "freezeAccountingPeriod", -2, QT_TRANSLATE_NOOP("storedProcErrorLookup", "Cannot freeze this Accounting Period "
 				     "because it is already frozen."), 0, "" },
 
+  { "getLotSerialIds", -1, QT_TRANSLATE_NOOP("storedProcErrorLookup", "pOrdHeadId OR pOrdItemId param is required "
+             "when passing pOrdType."), 0, "" },
+
   { "insertGLTransaction", -1, QT_TRANSLATE_NOOP("storedProcErrorLookup", "G/L Transaction can not be posted because "
              "the debit and credit accounts reference two different companies."), 0, "" },
   { "insertGLTransaction", -2, QT_TRANSLATE_NOOP("storedProcErrorLookup", "Cannot post a G/L transaction to a "
